40eca0cc8b938c27ef20db141f7a2533f9e38b2a
StuartFarmer <email@stuartfarmer.me>
2017-12-13 10:39:28 -0800

    database refreshes

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import sqlite3
+import rsa
+import os
 
 REFRESH_DB = True
+DB_NAME = 'blockchain.db'
+DB_PATH = os.path.join(os.getcwd(), DB_NAME)
 
-connection = sqlite3.connect('blockchain.db')
+if REFRESH_DB:
+	os.remove(DB_PATH)
+
+connection = sqlite3.connect(DB_NAME)
 cursor = connection.cursor()
 
-c.execute('''CREATE TABLE wallets 
+cursor.execute('''CREATE TABLE wallets 
 	(key text, 
 	balance real)'''
 )
 
+def new_wallet():
+	return rsa.newkeys(512, poolsize=8)
+
+(pubkey, privkey) = new_wallet()
+print(pubkey)
