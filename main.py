a03a95ee71367677572af5ad41a2ec3003ea9950
StuartFarmer <email@stuartfarmer.me>
2017-12-13 10:54:17 -0800

    ecdsa creation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
-import rsa
 import os
+from ecdsa import SigningKey
 
 REFRESH_DB = True
 DB_NAME = 'blockchain.db'
@@ -17,8 +17,28 @@ cursor.execute('''CREATE TABLE wallets
 	balance real)'''
 )
 
+def generate_keys():
+	# generates a tuple keypair
+	sk = SigningKey.generate()
+	vk = sk.get_verifying_key()
+	return (sk, vk)
+
+def keys_to_format(s, v):
+	# turns binary data into desired format
+	s = s.to_string()
+	v = v.to_string()
+	return (s.hex(), v.hex())
+
+def format_to_keys(s, v):
+	# returns the human readable format to bytes for processing
+	return(bytes.fromhex(s), bytes.fromhex(v))
+
 def new_wallet():
-	return rsa.newkeys(512, poolsize=8)
+	# interface to creating a new wallet
+	s, v = generate_keys()
+	return keys_to_format(s, v)
+
 
 (pubkey, privkey) = new_wallet()
 print(pubkey)
+print(privkey)
