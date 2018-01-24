59f61f608117462769c9d5e89bcb328a96ccd0e4
StuartFarmer <email@stuartfarmer.me>
2018-01-24 12:14:29 -0800

    masternodes are like a RESTful endpoint now

--- cilantro/networking/masternode.py
+++ cilantro/networking/masternode.py
@@ -6,8 +6,22 @@ import zmq
 app = Flask(__name__)
 api = Api(app)
 
+context = zmq.Context()
+publisher = context.socket(zmq.PUB)
+
 class Transactions(Resource):
     def post(self):
+        # serialize this bad boy
+
+        # send the message over zmq
+        try:
+            publisher.bind('tcp://127.0.0.1:9999')
+            publisher.send(b'test_message')
+        except:
+            return {'error' : 'response'}
+        finally:
+            publisher.unbind()
+
         return {'hello': 'world'}
 
 api.add_resource(Transactions, '/')
