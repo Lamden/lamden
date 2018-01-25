b8fbf292863dfd29122ea7f5a5d97aa130a4ff81
StuartFarmer <email@stuartfarmer.me>
2018-01-24 16:04:23 -0800

    up to date

--- cilantro/networking/masternode.py
+++ cilantro/networking/masternode.py
@@ -2,6 +2,15 @@ from flask import Flask, request, jsonify
 from cilantro.serialization import JSONSerializer
 import zmq
 
+'''
+    Masternode
+    These are the entry points to the blockchain and pass messages on throughout the system. They are also the cold
+    storage points for the blockchain once consumption is done by the network.
+    
+    They have no say as to what is 'right,' as governance is ultimately up to the network. However, they can monitor
+    the behavior of nodes and tell the network who is misbehaving. 
+'''
+
 class Masternode(object):
     def __init__(self, host='127.0.0.1', port='9999', serializer=JSONSerializer):
         self.host = host
@@ -9,13 +18,15 @@ class Masternode(object):
         self.serializer = serializer
 
         self.context = zmq.Context()
-        self.publisher = self.context.socket(zmq.SUB)
+        self.publisher = self.context.socket(zmq.PUB)
 
         self.url = 'tcp://{}:{}'.format(self.host, self.port)
 
     def process_tranasaction(self, data=None):
+        d = None
         try:
             d = self.serializer.serialize(data)
+            print(d)
         except:
             return {'status': 'Could not serialize transaction' }
 
@@ -25,6 +36,7 @@ class Masternode(object):
             print('ay')
         except Exception as e:
             print(e)
+            return {'status': 'Could not send transaction'}
         finally:
             self.publisher.unbind(self.url)
 
--- cilantro/serialization/json_serializer.py
+++ cilantro/serialization/json_serializer.py
@@ -2,19 +2,24 @@ from cilantro.serialization import Serializer
 import json
 import zmq
 
+'''
+    JSONSerializer
+    Takes bytes on the wire and transforms them into JSON objects to send through ZMQ
+    Not for production, but good for testing.
+'''
 class JSONSerializer(Serializer):
     @staticmethod
     def serialize(b: bytes):
         try:
-            return json.dumps(b.decode())
+            return json.loads(b.decode())
         except Exception as e:
             print(e)
-            return 0
+            return { 'error' : 'error' }
 
     @staticmethod
-    def deserialize(s: bytes):
-        return json.loads(s)
+    def deserialize(d: dict):
+        return json.dumps(d)
 
     @staticmethod
-    def send(d, p: zmq.Context):
-        p.send_json(d.decode("utf-8"))
+    def send(d: dict, p: zmq.Context):
+        p.send_json(d)
