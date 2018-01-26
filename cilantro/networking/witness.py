5e81f1d25cfc13bb5b63539a2837fa4b15cc166b
Denton <Denton24646@gmail.com>
2018-01-25 22:00:24 -0500

    witness
    built out witness functionality further

--- cilantro/networking/witness.py
+++ cilantro/networking/witness.py
@@ -1,5 +1,6 @@
 import zmq
 from cilantro.proofs.pow import fishfish
+from cilantro.serialization import json_serializer
 
 HOST = '127.0.0.1'
 PORT = '4444'
@@ -15,18 +16,21 @@ transaction_length = 32 # standard length for transaction data that can be used 
 witness.setsockopt_string(zmq.SUBSCRIBE, transaction_filter, transaction_length) # generate witness setting with filters to receive tx data only, of the right size, to avoid spam
 
 # include safeguard to make sure witness and masternode start at the same time and no packets are lost
+# add proxy/broker based solution to ensure dynamic discovery between masternodes and witnesses - solved via masternode acting as bootnode
 
 
 def confirmed_transaction_routine():
+    def send_to_delegates():
+        pass
     pass
 
 
 while True:
-    tx = witness.recv_string()
+    tx = witness.recv_string(zmq.ZMQ_DONTWAIT)
     if tx != -1:
-        if fishfish.TwofishPOW.check(tx):
+        raw_tx = json_serializer.JSONSerializer.deserialize(tx)
+        if fishfish.TwofishPOW.check(raw_tx, raw_tx.payload['metadata']['proof']):
             confirmed_transaction_routine()
-
 
 
 
