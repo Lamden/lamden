0e3616cbc25f75b3d99f89be3c8d27eca0d76ea6
Stuart Farmer <stuart@lamden.io>
2018-01-31 18:34:25 -0800

    Update test_witness.py

--- tests/cilantro/networking/test_witness.py
+++ tests/cilantro/networking/test_witness.py
@@ -1,13 +1,45 @@
 from unittest import TestCase
 from unittest.mock import Mock, MagicMock
-from cilantro.networking.witness import Witness
+from cilantro.networking import Witness
+from cilantro.networking import Masternode
+import asyncio
+class Mock_Masternode(Masternode):
+    def __init__(self, number_before_kill=10):
+        self.life = number_before_kill
+        super().__init__()
+
+    def process_transaction(self, tx=None):
+        self.publisher.bind(self.url)
+
+        if self.life <= 0:
+            self.publisher.send(tx)
+        else:
+            self.publisher.send(b'999')
+
+        self.life -= 1
+        self.publisher.unbind(self.url)
 
 class TestWitness(TestCase):
-    def setUp(self):
-        self.testwitness = Witness()
+    def test_async(self):
+        w = Witness()
+        m = Mock_Masternode()
+        loop = asyncio.get_event_loop()
+        a = loop.create_task(m.process_transaction(tx={'hello'}))
+        b= loop.create_task(w.accept_incoming_transactions())
+        loop.run_until_complete([ a, b ])
 
-    def basictest(self):
-        print(self.testwitness.masternodes, self.testwitness.delegates)
 
-    def tearDown(self):
-        self.testwitness.dispose()
+
+# unittest where mock masternode can send fake requests to witness
+
+
+
+'''
+    loop.run_until_complete([
+        masternode_publish,
+        witness_subscribe
+    ])
+    
+    masternode sends kill transaction b'999,' b'deadbeef'
+    
+'''
