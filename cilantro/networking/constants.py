9d9d4980314be139185b9037378b354b87b1ea7c
Davis Haba <dhaba@groupon.com>
2018-02-19 11:55:39 -0800

    - Added auto flush timer for queue

--- cilantro/networking/constants.py
+++ cilantro/networking/constants.py
@@ -1,5 +1,6 @@
 MAX_REQUEST_LENGTH = 100000
 MAX_QUEUE_SIZE = 4
+QUEUE_AUTO_FLUSH_TIME = 1.0
 
 TX_STATUS = {'SUCCESS': '{} successfully published to the network',
                'INVALID_TX_SIZE': 'transaction exceeded max size',
