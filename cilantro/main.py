f5f38d96ab161fa512ce8637016a78a06cbb7b5e
StuartFarmer <email@stuartfarmer.me>
2018-02-19 12:40:15 -0800

    xxtea implemented over ed25519

--- cilantro/main.py
+++ cilantro/main.py
@@ -3,19 +3,30 @@ import os
 from getpass import getpass
 from cilantro.wallets import ED25519Wallet
 from datetime import datetime
+import xxtea
 
 UNLOCKED_WALLET = None
 
-import nacl.secret
-import nacl.utils
 
 async def cli_loading_animation():
     pass
+
 
 def pad_bytes(b):
     while len(b) < 32:
         b += b'0'
     return b
+
+
+def format_password(p):
+    password = p.encode()[:32]
+    password = pad_bytes(password)
+    return password
+
+
+def password_as_nonce(p):
+    assert len(p) >= 24, 'Provided password is not long enough. Must be at least 24 bytes.'
+    return p[:24]
 
 @click.command()
 @click.option('--file_dir', default=None)
@@ -27,16 +38,12 @@ def new_wallet(file_dir):
     password = getpass('Enter a password to secure the wallet: ')
 
     # size password to 32 bytes for ed25519 encryption
-    password = password.encode()[:32]
-    password = pad_bytes(password)
-    print(password)
 
-    box = nacl.secret.SecretBox(password)
 
     wallet = ED25519Wallet.new()
     signing_key = bytes.fromhex(wallet[0])
 
-    encrypted_wallet = box.encrypt(signing_key)
+    encrypted_wallet = xxtea.encrypt(signing_key, password)
 
     print(encrypted_wallet)
 
@@ -49,6 +56,18 @@ def new_wallet(file_dir):
     with open(os.path.join(file_dir, file_name), 'wb') as f:
         f.write(encrypted_wallet)
 
+
+def unlock_wallet():
+
+    with open(os.path.join(os.path.join(os.getcwd(), 'test.tau')), 'rb') as f:
+        encrypted_wallet = f.read()
+
+    password = getpass('Enter the password to unlock the wallet: ')
+
+    signing_key = xxtea.decrypt(encrypted_wallet, password)
+
+    print(signing_key)
+
 @click.command()
 @click.argument('address')
 @click.argument('amount')
@@ -57,4 +76,5 @@ def send():
 
 
 if __name__ == '__main__':
-    new_wallet()
+    #new_wallet(None)
+    unlock_wallet()
