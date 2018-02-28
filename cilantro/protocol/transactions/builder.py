1db82c1f6d6f133ac66dd845b32ee484ca290a75
StuartFarmer <email@stuartfarmer.me>
2018-02-28 10:49:30 -0800

    all constant.py files deleted

--- cilantro/protocol/transactions/builder.py
+++ cilantro/protocol/transactions/builder.py
@@ -1,16 +1,16 @@
 from .constants import *
-
+from cilantro import Constants
 
 class TransactionBuilder:
     @classmethod
     def sign(cls, signing_key, data):
-        return WALLET.sign(signing_key, data)
+        return Constants.Protocol.Wallets.sign(signing_key, data)
 
     @classmethod
     def seal(cls, data):
-        return PROOF.find(data)
+        return Constants.Protocol.Proofs.find(data)
 
     @classmethod
     def build(cls, data):
-        assert VanillaInterpreter.is_valid_transaction_type(data)
+        assert Constants.Protocol.Interpreters.is_valid_transaction_type(data)
         pass
--- cilantro/protocol/transactions/constants.py
+++
@@ -1,9 +0,0 @@
-from cilantro.protocol.serialization import JSONSerializer
-from cilantro.protocol.proofs import SHA3POW
-from cilantro.protocol.wallets import ED25519Wallet
-from cilantro.protocol.interpreters import vanilla as VanillaInterpreter
-
-WALLET = ED25519Wallet
-SERIALIZER = JSONSerializer
-PROOF = SHA3POW
-INTERPRETER = VanillaInterpreter
