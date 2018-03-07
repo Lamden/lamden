031a93fc1fdabdb0be5bfea2c6491f58bd072263
StuartFarmer <email@stuartfarmer.me>
2018-03-06 17:02:37 -0800

    async get req for consensus on delegates

--- playground/consensus-playground.py
+++ playground/consensus-playground.py
@@ -3,6 +3,13 @@ from cilantro.models import MerkleTree
 from cilantro.protocol.wallets import ED25519Wallet
 import hashlib
 
+from multiprocessing import Process
+import zmq
+from cilantro import Constants
+import asyncio
+from aioprocessing import AioPipe
+from pprint import pprint
+
 '''
     For consensus, the masternode gets the following from the delegates:
     The merkle tree as a list
@@ -26,13 +33,82 @@ n = MerkleTree(fake_txs)
 # generate the delegates with new wallets
 delegates = [ED25519Wallet.new() for i in range(64)]
 
-print('\n===MERKLE TREE HASHED===')
-h = hashlib.sha3_256()
-[h.update(mm) for mm in m.nodes]
-print(h.digest().hex())
+connection_list = ['inproc://{}'.format(k[1]) for k in delegates]
 
-print('\n===ENTIRE MERKLE TREE===')
-[print(mm.hex()) for mm in m.nodes]
 
-print('\n===SIGNATURE OF MERKLE HASH===')
-[print(ED25519Wallet.sign(k[0], h.digest())) for k in delegates]
+def print_stuff():
+    pprint(connection_list)
+
+    print('\n===MERKLE TREE HASHED===')
+    h = hashlib.sha3_256()
+    [h.update(mm) for mm in m.nodes]
+    print(h.digest().hex())
+
+    print('\n===ENTIRE MERKLE TREE===')
+    [print(mm.hex()) for mm in m.nodes]
+
+    print('\n===SIGNATURE OF MERKLE HASH===')
+    [print(ED25519Wallet.sign(k[0], h.digest())) for k in delegates]
+
+signature_list = []
+
+def request_from_all_nodes():
+    #asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+
+    context = zmq.Context()
+
+    # replace with while signature_list length < 1/2 delegates
+    # choose a random connection and request a response
+    # verify that the signature is indeed accurate
+    # add that signature to a list of signatures
+    # stop when all of the signatures have been selected
+
+    delegates_to_request = list(connection_list)
+
+    async def get_message(future, connection):
+        request_socket = context.socket(socket_type=zmq.REQ)
+        request_socket.connect(connection)
+        request_socket.send(b'')
+
+        await asyncio.sleep(1)
+        future.set_result(b'yay')
+
+        request_socket.disconnect(connection)
+
+    def verify_signature(future):
+        signature_list.append(future.result())
+
+    futures = [asyncio.Future() for _ in range(len(delegates_to_request))]
+
+    [f.add_done_callback(verify_signature) for f in futures]
+
+    tasks = [get_message(*a) for a in zip(futures, delegates_to_request)]
+
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+def respond():
+    loop = asyncio.get_event_loop()
+
+    context = zmq.Context()
+
+    async def get_message(future, connection):
+        response_socket = context.socket(socket_type=zmq.REQ)
+        response_socket.bind(connection)
+        await response_socket.recv()
+        response_socket.send(b'signature')
+
+        future.set_result(b'yay')
+
+        response_socket.disconnect(connection)
+
+from multiprocessing import Process
+from threading import Thread
+p = Thread(target=request_from_all_nodes)
+
+# request_from_all_nodes()
+print(signature_list)
+
+class ZMQLoop(Process):
+    self.parent_pipe, self.child_pipe = AioPipe()
