71f9c90ef8880d9b583a94aab3650215c5e09583
StuartFarmer <email@stuartfarmer.me>
2018-03-14 16:32:07 -0700

    cleaned interpreter

--- cilantro/nodes/delegate/db/backend.py
+++ cilantro/nodes/delegate/db/backend.py
@@ -1,8 +1,12 @@
 import plyvel
+from cilantro.models.utils import int_to_decimal
+from cilantro import Constants
 
 SEPARATOR = b'/'
-STATE = b'state'
+SCRATCH = b'scratch'
 BALANCES = b'balances'
+TXQ = b'txq'
+PATH = '/tmp/cilantro'
 
 
 class Backend:
@@ -20,7 +24,7 @@ class Backend:
 
 
 class LevelDBBackend(Backend):
-    def __init__(self, path):
+    def __init__(self, path=PATH):
         self.path = path
 
     def get(self, table: bytes, key: bytes):
@@ -63,7 +67,7 @@ class TransactionQueue:
     def __init__(self, backend):
         self.backend = backend
         self.size = 0
-        self.table_name = b'txq'
+        self.table_name = TXQ
 
     def push(self, tx):
         self.size += 1
@@ -85,8 +89,10 @@ class StateQuery:
     def __init__(self, table_name, backend):
         self.table_name = table_name
         self.backend = backend
-        self.state_table = SEPARATOR.join([STATE, self.table_name])
-        self.txq_table = SEPARATOR.join([b'txq', self.table_name])
+        self.scratch_table = SEPARATOR.join([SCRATCH, self.table_name])
+
+        self.txq = TransactionQueue(backend=self.backend)
+        self.txq_table = SEPARATOR.join([TXQ, self.table_name])
 
     def process_tx(self, tx: dict):
         raise NotImplementedError
@@ -100,12 +106,12 @@ class StandardQuery(StateQuery):
     StandardQuery
     Automates the state and txq modifications for standard transactions
     """
-    def __init__(self, table_name=BALANCES, backend=LevelDBBackend('/tmp/cilantro')):
+    def __init__(self, table_name=BALANCES, backend=LevelDBBackend()):
         super().__init__(table_name=table_name, backend=backend)
 
     def get_balance(self, address):
-        if self.backend.exists(self.state_table, address.encode()):
-            return int_to_decimal(E.int(self.backend.get(self.state_table, address.encode())))
+        if self.backend.exists(self.scratch_table, address.encode()):
+            return int_to_decimal(E.int(self.backend.get(self.scratch_table, address.encode())))
         else:
             return int_to_decimal(E.int(self.backend.get(self.table_name, address.encode())))
 
@@ -130,7 +136,12 @@ class StandardQuery(StateQuery):
             new_receiver_balance = receiver_balance + tx.amount
             new_receiver_balance = self.encode_balance(new_receiver_balance)
 
-            self.backend.set(self.state_table, tx.sender.encode(), new_sender_balance)
-            self.backend.set(self.state_table, tx.receiver.encode(), new_receiver_balance)
+            self.backend.set(self.scratch_table, tx.sender.encode(), new_sender_balance)
+            self.backend.set(self.scratch_table, tx.receiver.encode(), new_receiver_balance)
+
+            self.txq.push(tx)
+
+            return tx, (self.scratch_table, tx.sender.encode(), new_sender_balance), \
+                   (self.scratch_table, tx.receiver.encode(), new_receiver_balance)
         else:
-            return None
+            return None, None, None
