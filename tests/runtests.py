f35bf7b2b68614d9c302cdfee13825b0f742e5f9
Denton <Denton24646@gmail.com>
2018-05-12 11:28:45 -0400

    testsuite
    testsuite

---
+++ tests/runtests.py
@@ -0,0 +1,18 @@
+# import unittest
+#
+# from tests import constants, db, messages, nodes, protocol, utils
+#
+#
+# TEST_MODULES = [
+#     'tests.messages.consensus'
+# ]
+#
+# loader = unittest.TestLoader()
+# suite = unittest.TestSuite()
+#
+#
+# suite.addTests(loader.loadTestsFromModule(player))
+#
+#
+# runner = unittest.TextTestRunner(verbosity=3)
+# result = runner.run(suite)
