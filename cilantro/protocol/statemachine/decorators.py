f769898a29e217336060b4427b3073ab2c10a25c
Davis Haba <dhaba@groupon.com>
2018-05-17 13:11:48 -0700

    decent docstrings to state executors

--- cilantro/protocol/statemachine/decorators.py
+++ cilantro/protocol/statemachine/decorators.py
@@ -1,3 +1,15 @@
+"""
+----------------------------------------
+Input Decorators
+----------------------------------------
+
+Input decorators allow states to define logic for incoming messages from the ReactorDaemon. These messages can be
+envelopes from other actors, or timeout callbacks from unreceived replies.
+
+# TODO more explanations
+# TODO examples of how to use input decorators
+"""
+
 def input(msg_type):
     def decorate(func):
         func._recv = msg_type
@@ -17,3 +29,29 @@ def timeout(msg_type):
         func._timeout = msg_type
         return func
     return decorate
+
+
+"""
+----------------------------------------
+Transition Decorators
+----------------------------------------
+
+Transition decorators allow states to define logic surrounding state transitions. Methods can be decorated to execute
+some code whenever the defining state is transition into from another state, using enter_state(...), or transitioned 
+out of into another state, using exit_state(....).  
+
+For either decorator enter_state(...)/exit_state(...), if no arguement is specified then that method will act as a 
+'wildcard' and be called for ALL transitions, unless the state has another method that is decorated to handle a
+a particular state.
+
+# TODO clearer explanation
+# TODO examples 
+"""
+
+
+def enter_state(prev_state=None):
+    pass
+
+
+def exit_state(next_state=None):
+    pass
---
+++ tests/protocol/statemachine/test_state.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from
+
+"""
+So we basically want to test...
+
+1) input/input_request/timeout decorator 
+2) input/input_request/timeout decorator inheritance + polymorphism
+
+3) enter/exit called appropriately on transition
+4) state_enter/state_exit decorators 
+"""
