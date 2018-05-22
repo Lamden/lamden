e59f115b9272afa6409d917a051cc805436492ca
Davis Haba <dhaba@groupon.com>
2018-05-22 11:05:31 -0700

    god composer class

--- cilantro/utils/test/__init__.py
+++ cilantro/utils/test/__init__.py
@@ -1,3 +1,4 @@
 from cilantro.utils.test.picklable_mixin import PicklableMixin
 from cilantro.utils.test.mp_test import MPTesterBase, mp_testable
 from cilantro.utils.test.mp_test_case import MPTestCase
+from cilantro.utils.test.god import *
