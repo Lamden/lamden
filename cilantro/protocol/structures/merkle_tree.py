c264d7f9f480dddd3db54c722d07be95ed10826f
Davis Haba <davishaba@Daviss-MacBook-Pro.local>
2018-05-28 14:55:14 -0700

    merkle_tree.py docstrings

--- cilantro/protocol/structures/merkle_tree.py
+++ cilantro/protocol/structures/merkle_tree.py
@@ -32,13 +32,28 @@ class MerkleTree:
         return Hasher.hash_iterable(self.nodes, algorithm=Hasher.Alg.SHA3_256, return_bytes=True)
 
     @staticmethod
-    def verify_tree(tree_nodes: list, tree_hash: bytes):
-        nodes = MerkleTree.merklize(tree_nodes, hash_leaves=False)
+    def verify_tree(nodes: list, tree_hash: bytes):
+        """
+        Attempts to verify merkle tree represented implicitly by the list 'nodes'. The tree is valid if it maintains
+        the invariant that the value of each non-leaf node is the hash of its left child's value concatenated with
+        its right child's value.
+        :param nodes: The nodes in the tree, represented implicitly as a list
+        :param tree_hash: The expected hash of the merkle tree formed from nodes (the 'hash of a merkle tree' is the
+        value returned by the .hash_of_nodes method on this class)
+        :return: True if the tree is valid; False otherwise
+        """
+        nodes = MerkleTree.merklize(nodes, hash_leaves=False)
         h = Hasher.hash_iterable(nodes, algorithm=Hasher.Alg.SHA3_256, return_bytes=True)
         return h == tree_hash
 
     @staticmethod
     def merklize(leaves: list, hash_leaves=True) -> list:
+        """
+        Builds a merkle tree from leaves and returns the tree as a list (representing an implicitly stored binary tree)
+        :param leaves: The leaves to form a merkle tree from.
+        :param hash_leaves: True if the leaves should be hashed before building the tree.
+        :return: A list, which serves as an implicit representation of the merkle tree
+        """
         if hash_leaves:
             leaves = [MerkleTree.hash(bytes(l)) for l in leaves]
 
