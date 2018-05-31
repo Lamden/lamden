6ea1dfdc37f7fbb3ed61cde1dba8b146bcb4929c
Davis Haba <davis.haba76@gmail.com>
2018-05-30 20:37:01 -0700

    witness uses new state machine decorators

--- cilantro/nodes/witness/witness.py
+++ cilantro/nodes/witness/witness.py
@@ -16,12 +16,17 @@ from cilantro.db.db import VKBook
 """
 
 
+class Witness(NodeBase):
+    pass
+
+
 class WitnessBaseState(State):
     @input(TransactionBase)
     def recv_tx(self, tx: TransactionBase, envelope: Envelope):
         self.log.critical("Witness not configured to recv tx: {} with env {}".format(tx, envelope))
 
 
+@Witness.register_init_state
 class WitnessBootState(WitnessBaseState):
     """
     Witness boot state has witness sub to masternode and establish a pub socket and router socket
@@ -41,23 +46,14 @@ class WitnessBootState(WitnessBaseState):
         # Create publisher socket
         self.parent.composer.add_pub(ip=self.parent.ip)
 
-        # OLD CODE TODO remove below
-        # self.parent.composer.add_router(ip=self.parent.ip)
-        # self.parent.composer.add_pub(url=TestNetURLHelper.pubsub_url(self.parent.url))  TODO add this
-        # self.parent.composer.add_router(url=TestNetURLHelper.dealroute_url(self.parent.url))  TODO add this
-        #
-        # self.parent.composer.add_sub(url=TestNetURLHelper.pubsub_url(Constants.Testnet.Masternode.InternalUrl),
-        #                              filter=Constants.Testnet.ZmqFilters.WitnessMasternode)
-        # self.log.critical("Witness subscribing to URL: {}"
-        #                   .format(TestNetURLHelper.pubsub_url(Constants.Testnet.Masternode.InternalUrl)))
-
-        # Once done booting, transition into RunState
+        # Once done setting up sockets, transition to RunState
         self.parent.transition(WitnessRunState)
 
     def run(self):
         self.parent.transition(WitnessRunState)
 
 
+@Witness.register_state
 class WitnessRunState(WitnessBaseState):
     """
     Witness run state has the witness receive transactions sent from masternode
@@ -75,10 +71,9 @@ class WitnessRunState(WitnessBaseState):
     @input(ContractSubmission)
     def handle_contract_submission(self, contract: ContractSubmission, envelope: Envelope):
         self.log.critical("WITNESS GOT CONTRACT SUBMISSION {}".format(contract))
-
         self.parent.composer.send_pub_env(envelope=envelope, filter=Constants.ZmqFilters.WitnessDelegate)
 
 
-class Witness(NodeBase):
-    _INIT_STATE = WitnessBootState
-    _STATES = [WitnessBootState, WitnessRunState]
+# class Witness(NodeBase):
+#     _INIT_STATE = WitnessBootState
+#     _STATES = [WitnessBootState, WitnessRunState]
