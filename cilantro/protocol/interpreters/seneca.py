e2ebfb07b1eb83abca8ed3db55080547d781d261
Davis Haba <davis.haba76@gmail.com>
2018-05-31 15:08:36 -0700

    wip on seneca integrations

--- cilantro/protocol/interpreters/seneca.py
+++ cilantro/protocol/interpreters/seneca.py
@@ -2,8 +2,43 @@ from cilantro.protocol.interpreters.base import BaseInterpreter
 from cilantro.db import *
 from cilantro.messages import *
 
+import datetime
+
+import seneca
+import seneca.seneca_internal.storage.easy_db as t
+from seneca.seneca_internal.storage.mysql_executer import Executer
+
+
+def build_contract_table(ex):
+    contract_table = t.Table('smart_contracts',
+                    t.Column('contract_address', t.str_len(30), True),
+                    [
+                        t.Column('code_str', str),
+                        t.Column('author', t.str_len(60)),
+                        t.Column('execution_datetime', datetime),
+                        t.Column('execution_status', t.str_len(30)),
+                    ])
+
+    try:
+        contract_table.drop_table().run(ex)
+    except Exception as e:
+        if e.args[0]['error_code'] == 1051:
+            pass
+        else:
+            raise
+
+    contract_table.create_table().run(ex)
+
+    return contract_table
+
 
 class SenecaInterpreter(BaseInterpreter):
+
+    def __init__(self):
+        super().__init__()
+
+        self.ex = Executer.init_local_noauth_dev()
+        self.contract_table = build_contract_table(self.ex)
 
     def flush(self, update_state=True):
         """
@@ -20,7 +55,16 @@ class SenecaInterpreter(BaseInterpreter):
 
     def _interpret_submission(self, submission: ContractSubmission):
         self.log.debug("Interpreting contract submission: {}".format(submission))
-        pass
+
+        res = self.contract_table.insert([{
+            'contract_address': submission.user_id,
+            'code_str': submission.contract_code,
+            'author': submission.user_id,
+            'execution_datetime': None,
+            'execution_status': 'pending',
+        }]).run(self.ex)
+
+        self.log.debug("res: {}".format(res))
 
     def _interpret_contract(self, contract):
         raise NotImplementedError
