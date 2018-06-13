2a1ef4b1d9813d7b68b1b7809667d5c046335f8f
Davis Haba <davis.haba76@gmail.com>
2018-06-13 13:58:21 -0700

    skeleton for seed_balances

--- cilantro/db/tables.py
+++ cilantro/db/tables.py
@@ -1,17 +1,18 @@
 from cilantro.logger import get_logger
-import seneca.seneca_internal.storage.easy_db as t
-from seneca.seneca_internal.storage.mysql_executer import Executer
-from datetime import datetime
+import json, os
 
 
 log = get_logger("DB Creator")
 
 GENESIS_HASH = '0' * 64
 
+constitution_json = json.load(open(os.path.join(os.path.dirname(__file__), 'constitution.json')))
+
 
 def build_tables(ex, should_drop=True):
     from cilantro.db.contracts import build_contracts_table, seed_contracts
     from cilantro.db.blocks import build_blocks_table, seed_blocks
+    from cilantro.db.balances import seed_balances
 
     if should_drop:
         log.info("Dropping Seneca database")
@@ -22,8 +23,15 @@ def build_tables(ex, should_drop=True):
     contracts = build_contracts_table(ex, should_drop)
     blocks = build_blocks_table(ex, should_drop)
 
-    seed_contracts(ex, contracts)
-    seed_blocks(ex, blocks)
+    # Only seed database if we just dropped it
+    if should_drop:
+        # Seed tables created by Cilantro
+        seed_contracts(ex, contracts)
+        seed_blocks(ex, blocks)
+
+        # Seed smart contract tables
+        seed_balances(ex)
+
 
     tables = type('Tables', (object,), {'contracts': contracts, 'blocks': blocks})
 
