91991bbe5ff1e14094cc12755a649238621576d8
Davis Haba <davis.haba76@gmail.com>
2018-06-29 15:08:47 -0700

    state_update.py docstrings

--- cilantro/messages/consensus/state_update.py
+++ cilantro/messages/consensus/state_update.py
@@ -1,16 +1,15 @@
 from cilantro.messages import MessageBaseJson
-import json
-
-"""
-State Requests are sent from delegates to masternodes. Delegates use this to get the latest state of the block chain.
-A delegate may need to do this if it is:
- 1) out of consensus
- 2) bootstrapping their application
- 3) any connection issues that result in missed transactions (which will likely lead to case 1, but not exclusively)
-"""
+from typing import List
 
 
 class StateUpdateRequest(MessageBaseJson):
+    """
+    State Requests are sent from delegates to masternodes. Delegates use this to get the latest state of the block chain.
+    A delegate may need to do this if it is:
+     1) out of consensus
+     2) bootstrapping their application
+     3) any connection issues that result in missed transactions (which will likely lead to case 1, but not exclusively)
+    """
 
     B_NUM = 'block_num'
     B_HASH = 'block_hash'
@@ -31,32 +30,40 @@ class StateUpdateRequest(MessageBaseJson):
         return cls.from_data(data)
 
     @property
-    def block_num(self):
+    def block_num(self) -> int:
+        """
+        The block number to request data for
+        """
         return self._data.get(self.B_NUM)
 
     @property
-    def block_hash(self):
+    def block_hash(self) -> str:
+        """
+        The block hash to request data for
+        """
         return self._data.get(self.B_HASH)
 
 
 # TODO -- write unit tests for this guy
 class StateUpdateReply(MessageBaseJson):
+    """
+    A StateUpdateReply contains all the transactions in a particular block. This information is stored in the 'payload'
+    property, which represents the leaves of the block's merkle tree (stored as transaction binaries).
+    """
+
     B_NUM = 'block_num'
     B_HASH = 'block_hash'
     B_PAYLOAD = 'block_payload'
-    MERK_ROOT = 'merkle_root'
 
     def validate(self):
         pass
 
     @classmethod
-    def create(cls, merkle_root: bytes, payload: bytes, block_num=None, block_hash=None):
+    def create(cls, payload: list, block_num=None, block_hash=None):
         assert block_hash or block_num, "StateUpdateReply must be created with a block hash or block number"
 
-        data = {}
+        data = {cls.B_PAYLOAD: payload}
 
-        data[cls.B_PAYLOAD] = payload
-        data[cls.MERK_ROOT] = merkle_root
         if block_num:
             data[cls.B_NUM] = block_num
         if block_hash:
@@ -65,19 +72,24 @@ class StateUpdateReply(MessageBaseJson):
         return cls.from_data(data)
 
     @property
-    def current_block_num(self):
+    def block_num(self) -> int:
+        """
+        The block number to request data for
+        """
         return self._data.get(self.B_NUM)
 
     @property
-    def current_block_hash(self):
+    def block_hash(self) -> str:
+        """
+        The block hash to request data for
+        """
         return self._data.get(self.B_HASH)
 
     @property
-    def merkle_root(self):
-        return self._data[self.MERK_ROOT]
-
-    @property
-    def payload(self):
+    def payload(self) -> List[bytes]:
+        """
+        The block transaction payload. This is a list of transactions, which represented the leaves of a merkle tree.
+        """
         return self._data[self.B_PAYLOAD]
 
 
