55edecd0ab2ad235ea44b2ea9ae7df6d05814ccb
Falcon Wong <wywfalcon@gmail.com>
2018-07-09 19:34:53 -0700

    abstracting election and soon sc in an OO way

--- tests/contracts/test_election.py
+++ tests/contracts/test_election.py
@@ -3,10 +3,11 @@ from unittest import TestCase
 from cilantro.logger import get_logger
 from tests.contracts.smart_contract_testcase import *
 from seneca.execute_sc import execute_contract
+import seneca.smart_contract_user_libs.stdlib as std
+import time
 
 log = get_logger("TestElection")
 user_id = 'tester'
-POLICY = 'test_election'
 
 class TestElection(SmartContractTestCase):
     @contract('election')
@@ -19,14 +20,41 @@ class TestElection(SmartContractTestCase):
 
     @contract('election')
     def test_create(self, election):
-        election_id = election.create_election(user_id, POLICY, tags=['dogs', 'cats'])
+        election_id = election.create_election(std.timedelta(seconds=1), int)
         self.assertEqual(election_id, 1)
 
     @contract('election')
-    def test_close(self, election):
-        STATUS = election.enum()
-        election_id = election.create_election('{user_id}', '{POLICY}', tags=['dogs', 'cats'])
-        election.close_election(user_id, POLICY, STATUS.PASSED)
+    def test_create_fail(self, election):
+        with self.assertRaises(Exception) as context:
+            election_id = election.create_election(123, int)
+
+    @contract('election')
+    def test_get_election(self, election):
+        election_id = election.create_election(std.timedelta(seconds=37), int)
+        e = election.get_election(election_id)
+        self.assertEqual(e['opened_ts'], e['expire_on']-std.timedelta(seconds=37))
+
+    # TODO wait until Seneca support time comparison
+    # @contract('election')
+    # def test_get_election_failed(self, election):
+    #     election_id = election.create_election(std.timedelta(seconds=1), int)
+    #     time.sleep(1.5)
+    #     e = election.get_active_election(election_id, if_active=True)
+
+    @contract('election')
+    def test_pass(self, election):
+        election_id = election.create_election(std.timedelta(seconds=10), int)
+        election.pass_election(election_id)
+
+    @contract('election')
+    def test_fail(self, election):
+        election_id = election.create_election(std.timedelta(seconds=10), int)
+        election.fail_election(election_id)
+
+    @contract('election')
+    def test_cancel(self, election):
+        election_id = election.create_election(std.timedelta(seconds=10), int)
+        election.cancel_election(election_id)
 
 if __name__ == '__main__':
     unittest.main()
