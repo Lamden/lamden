0185acf6c3c7b46a47868390503416e1970cba1c
Stuart Farmer <stuart@lamden.io>
2018-07-25 17:31:28 -0700

    profiling

--- cilantro/utils/lprocess.py
+++ cilantro/utils/lprocess.py
@@ -1,6 +1,29 @@
 from cilantro.logger import get_logger
-from multiprocessing import Process
 import traceback
+from pympler import muppy, summary
+from multiprocessing import Process
+import sys
+from io import StringIO
+import os
+
+
+def mup_dat(filename):
+    oo = muppy.get_objects()
+    s = summary.summarize(oo)
+
+    # Intercept stdout
+    old_stdout = sys.stdout
+    
+    summary.print_(s)
+
+
+def get_filename(proc_name):
+    prefix = os.getenv('HOSTNAME', '')
+    if prefix:
+        return prefix + '_' + proc_name
+    else:
+        return proc_name
+
 
 
 DELIM_LEN = 60
@@ -14,6 +37,8 @@ class LProcess(Process):
         log.info("---> {} Starting --->".format(self.name))
 
         try:
+            log.critical("\nMup dat before running process {}\n".format(self.name))
+            mup_dat()
             super().run()
         except Exception as e:
             err_msg = '\n' + OUTER_DELIM * DELIM_LEN
@@ -25,4 +50,6 @@ class LProcess(Process):
             log.error(err_msg)
         finally:
             log.info("<--- {} Terminating <---".format(self.name))
+            log.critical("\nMup dat after teardown of {}\n".format(self.name))
+            mup_dat()
             # TODO -- signal to parent to call .join() on this process and clean up nicely
