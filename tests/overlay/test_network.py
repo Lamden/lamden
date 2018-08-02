174c1b98fb99f04b0c0572c91c9091dc6a23512c
Falcon Wong <wywfalcon@gmail.com>
2018-08-02 09:26:06 -0700

    stopping everything first

--- tests/overlay/test_network.py
+++ tests/overlay/test_network.py
@@ -138,9 +138,10 @@ class TestNetwork(TestCase):
             conn = self.a_net.connect_to_neighbor(self.b_net.node)
             time.sleep(0.1)
             self.b_net.stop()
+            stop(self)
             time.sleep(0.1)
             self.assertEqual(self.a_net.connections, {})
-            stop(self)
+
 
         t = Timer(0.01, run, [self])
         t.start()
