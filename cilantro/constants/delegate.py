daddbcc8dac6943b02f3c05d9227d1bf657e1ec9
Davis Haba <davis.haba76@gmail.com>
2018-08-02 22:09:48 -0700

    delegate constants

---
+++ cilantro/constants/delegate.py
@@ -0,0 +1,6 @@
+# Number of seconds Delegate will wait in BootState before he calls it quits
+BOOT_TIMEOUT = 60
+
+# Number of nodes Delegate must be able to connect to before leaving BootState
+BOOT_REQUIRED_MASTERNODES = 1
+BOOT_REQUIRED_WITNESSES = 1
