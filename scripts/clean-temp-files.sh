2a9566de5dec432147ee877edfdae0dc33619cd3
Davis Haba <davis.haba76@gmail.com>
2018-08-06 17:55:50 -0700

    update clean temp file script

--- scripts/clean-temp-files.sh
+++ scripts/clean-temp-files.sh
@@ -1,5 +1,6 @@
+cd ..
 find . -name '*-ReactorIPC-*' -delete
-find . -name '*TestableProcess_*' -delete
+find . -name '*TestableProcess-*' -delete
 find . -name '*-ReactorIPC-*' -delete
 find . -name 'LProcess-*' -delete
 find . -name '*tmp_exec_code_*' -delete
