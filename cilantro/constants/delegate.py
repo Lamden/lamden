03f703d5c771ba4567581cec5f4600264a21f3fd
Falcon Wong <wywfalcon@gmail.com>
2018-08-13 13:25:26 -0700

    add profiling in lprocess

--- cilantro/constants/delegate.py
+++ cilantro/constants/delegate.py
@@ -6,6 +6,9 @@ CONSENSUS_TIMEOUT = 24
 # Number of nodes Delegate must be able to connect to before leaving BootState
 BOOT_REQUIRED_MASTERNODES = 1
 BOOT_REQUIRED_WITNESSES = 1
+TOP_DELEGATES = 32
+NODES_REQUIRED_CONSENSUS = 22
+RELIABILITY_FACTOR = 5
 
 BLOCK_REQ_TIMEOUT = 5  # Timeout for BlockMetaDataRequests
 TX_REQ_TIMEOUT = 8  # Timeout for TransactionRequests
