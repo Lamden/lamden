5277629967551376bc4c8d27cacebf0ad7d9bf1d
Davis Haba <davis.haba76@gmail.com>
2018-08-24 12:30:47 -0700

    comments

--- cilantro/nodes/masternode/transaction_batcher.py
+++ cilantro/nodes/masternode/transaction_batcher.py
@@ -6,11 +6,14 @@ import asyncio
 class TransactionBatcher(Worker):
 
     def setup(self):
+        # TODO create PUB socket here to send stuff out to witnesses. remove the other pub socket created in masternode state machine
         asyncio.ensure_future(self.compose_transactions())
 
     async def compose_transactions(self):
         self.log.important3("STARTING TRANSACTION BATCHER")
         while True:
-            self.log.important("(live from raghu proc) Current queue size is {}".format(self.queue.qsize()))
+            self.log.important("Current queue size is {}".format(self.queue.qsize()))
+            # TODO batch these things and send them out. self.queue is a multiprocessing.Queue object,
+            # and all sanic workers are stuffing the data get get from rest requests into this queue
             await asyncio.sleep(2)
 
