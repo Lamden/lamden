493dbd65c9f5c98e707b173321c88f1029ab3270
Davis Haba <davis.haba76@gmail.com>
2018-09-18 16:15:54 -0700

    change CLIENT_SETUP_TIMEOUT from 10 to 20

--- cilantro/constants/overlay_network.py
+++ cilantro/constants/overlay_network.py
@@ -6,4 +6,4 @@ MAX_PEERS = 64
 
 AUTH_TIMEOUT = 7 # use 5 in prod
 RPC_TIMEOUT = 10 # use 3 in prod
-CLIENT_SETUP_TIMEOUT = 10  # How long we should wait for a ready signal from OVERLAY_CLIENT until we timeout
+CLIENT_SETUP_TIMEOUT = 20  # How long we should wait for a ready signal from OVERLAY_CLIENT until we timeout
