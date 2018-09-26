b25141be3fc6a3223fcf1f4f6001507b7ae1cb4b
Davis Haba <davis.haba76@gmail.com>
2018-09-26 12:52:42 -0700

    wipndip

--- cilantro/constants/overlay_network.py
+++ cilantro/constants/overlay_network.py
@@ -2,7 +2,7 @@ ALPHA = 3
 KSIZE = 20
 MAX_PEERS = 64
 
-AUTH_TIMEOUT = 7  # use 5 in prod
+AUTH_TIMEOUT = 10  # use 5 in prod
 RPC_TIMEOUT = 10  # use 3 in prod
-CLIENT_SETUP_TIMEOUT = 20  # How long OverlayClient should wait for a rdy sig from the OverlayServer until we timeout
+CLIENT_SETUP_TIMEOUT = 8  # How long OverlayClient should wait for a rdy sig from the OverlayServer until we timeout
 
