7eb305cef2f1c89dfab820c1d8a62a34ad0ab97f
Davis Haba <davis.haba76@gmail.com>
2018-10-03 10:23:19 -0700

    router client and server appears to work. integration test for it.

--- cilantro/utils/test/mp_testables.py
+++ cilantro/utils/test/mp_testables.py
@@ -13,6 +13,7 @@ from cilantro.protocol.overlay.interface import OverlayServer
 from cilantro.utils.lprocess import LProcess
 from cilantro.storage.db import DB
 from cilantro.utils.test.pubsub_auth import PubSubAuthTester
+from cilantro.utils.test.router_auth import RouterAuthTester
 import asyncio
 import zmq.asyncio
 import os
@@ -30,6 +31,22 @@ class MPPubSubAuth(MPTesterBase):
         overlay_proc.start()
 
         obj = PubSubAuthTester(sk, name=name, loop=loop)
+
+        return obj, loop, []
+
+
+@mp_testable(RouterAuthTester)
+class MPRouterAuth(MPTesterBase):
+    @classmethod
+    def build_obj(cls, sk, name='') -> tuple:
+        loop = asyncio.get_event_loop()
+        asyncio.set_event_loop(loop)
+
+        # Start Overlay Process
+        overlay_proc = LProcess(target=OverlayServer, args=(sk,))
+        overlay_proc.start()
+
+        obj = RouterAuthTester(sk, name=name, loop=loop)
 
         return obj, loop, []
 
