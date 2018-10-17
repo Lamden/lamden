c239435d6b373931ecc9285d1e848cc0211d1415
Falcon Wong <wywfalcon@gmail.com>
2018-10-17 16:56:02 -0700

    try sleep

--- tests/integration/networking/test_large_network.py
+++ tests/integration/networking/test_large_network.py
@@ -84,7 +84,7 @@ class TestLargeNetwork(MPTestCase):
         time.sleep(1)
 
         mn_0 = MPPubSubAuth(sk=TESTNET_MASTERNODES[0]['sk'], name='[node_1]MN_0', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
-        time.sleep(3)  # Pause after first MN boots (so we are extra sure he will be available for discovery)
+        time.sleep(4)  # Pause after first MN boots (so we are extra sure he will be available for discovery)
         mn_1 = MPPubSubAuth(sk=TESTNET_MASTERNODES[1]['sk'], name='[node_2]MN_1', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
 
         wit_0 = MPPubSubAuth(sk=TESTNET_WITNESSES[0]['sk'], name='[node_3]WITNESS_0', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
@@ -95,7 +95,7 @@ class TestLargeNetwork(MPTestCase):
         del_2 = MPPubSubAuth(sk=TESTNET_DELEGATES[2]['sk'], name='[node_7]DELEGATE_2', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
         del_3 = MPPubSubAuth(sk=TESTNET_DELEGATES[3]['sk'], name='[node_8]DELEGATE_3', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
 
-        time.sleep(16*CI_FACTOR)  # Nap while nodes hookup
+        time.sleep(20*CI_FACTOR)  # Nap while nodes hookup
 
         all_nodes = (mn_0, mn_1, wit_0, wit_1, del_0, del_1, del_2, del_3)
         all_vks = (TESTNET_MASTERNODES[0]['vk'], TESTNET_MASTERNODES[1]['vk'], TESTNET_WITNESSES[0]['vk'],
@@ -114,13 +114,13 @@ class TestLargeNetwork(MPTestCase):
                 if vk == node_vk: continue
                 n.connect_sub(vk=vk)
 
-        time.sleep(16*CI_FACTOR)  # Allow time for VK lookups
+        time.sleep(20*CI_FACTOR)  # Allow time for VK lookups
 
         # Make each node pub a msg
         for n in all_nodes:
             n.send_pub("hi from {} with ip {}".format(n.name, n.ip).encode())
 
-        self.start(timeout=10*CI_FACTOR)
+        self.start(timeout=30*CI_FACTOR)
 
 
 if __name__ == '__main__':
