811b284b49c3512d6ae587db8569fa752bf00830
Falcon Wong <wywfalcon@gmail.com>
2018-11-05 14:09:07 -0800

    wip

--- cilantro/nodes/factory.py
+++ cilantro/nodes/factory.py
@@ -1,9 +1,9 @@
 from cilantro.nodes.masternode.masternode import Masternode
 from cilantro.nodes.delegate.delegate import Delegate
 from cilantro.nodes.witness.witness import Witness
-from cilantro.storage.db import DB
+from cilantro.storage.contracts import seed_contracts
 from cilantro.storage.sqldb import SQLDB
-
+from seneca.engine.interface import SenecaInterface
 
 def _build_block_schema(should_reset):
     SQLDB.setup_db()
@@ -12,26 +12,25 @@ def _build_block_schema(should_reset):
 
 
 class NodeFactory:
+    @staticmethod
+    def _reset_db():
+        with SenecaInterface() as interface:
+            interface.r.flushall()
+        seed_contracts()
 
     @staticmethod
-    def run_masternode(signing_key, ip, name='Masternode', reset_db=False):
-        with DB(should_reset=reset_db) as db:
-            pass
+    def run_masternode(signing_key, ip, name='Masternode', reset_db=False):\
+        if reset_db: NodeFactory._reset_db()
         _build_block_schema(reset_db)
 
         mn = Masternode(ip=ip, name=name, signing_key=signing_key)
 
     @staticmethod
     def run_witness(signing_key, ip, name='Witness', reset_db=False):
-        with DB(should_reset=reset_db) as db:
-            pass
-
+        if reset_db: NodeFactory._reset_db()
         w = Witness(ip=ip, name=name, signing_key=signing_key)
 
     @staticmethod
     def run_delegate(signing_key, ip, name='Delegate', reset_db=False):
-        with DB(should_reset=reset_db) as db:
-            pass
-
+        if reset_db: NodeFactory._reset_db()
         d = Delegate(ip=ip, name=name, signing_key=signing_key)
-
