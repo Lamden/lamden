a58252f4e64f9253a86956959170482487ccd84e
Falcon Wong <wywfalcon@gmail.com>
2018-11-05 18:07:56 -0800

    get rid of the contract executor test

--- tests/groups.py
+++ tests/groups.py
@@ -13,7 +13,6 @@ TODO -- better documentation on whats going on here
 
 PROTOCOL_TESTS = [
     'tests.unit.protocol.overlay',
-    'tests.unit.protocol.interpreter',
     'tests.unit.protocol.structures',
     'tests.unit.protocol.states',
     'tests.unit.protocol.wallets'
