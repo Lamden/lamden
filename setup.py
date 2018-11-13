fee123108b1c4a2d7b79c5d500e5dcf2e291a632
Stuart Farmer <stuart@lamden.io>
2018-11-13 12:47:03 -0800

    first push for basic cli entry

--- setup.py
+++ setup.py
@@ -6,14 +6,40 @@ setup(
     name='cilantro',
     version=__version__,
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=[
+        'aenum==2.0.8',
+        'aiodns==1.1.1',
+        'aiohttp',
+        'aioprocessing==1.0.1',
+        'coloredlogs',
+        'Cython',
+        'idna==2.6',
+        'mysql-connector-python',
+        'ntplib==0.3.3',
+        'pycapnp==0.6.3',
+        '#PyNaCl==1.2.1',
+        'pytest==3.5.0',
+        'pytest-aiohttp==0.3.0',
+        '#pyzmq==17.0.0',
+        'requests==2.18.4',
+        'six==1.11.0',
+        'urllib3==1.22',
+        'uvloop==0.9.1',
+        'u-msgpack-python==2.5.0',
+        'yarl==1.1.0',
+        'seneca'
+    ],
+    dependency_links=[
+        "git://github.com/Lamden/seneca.git@dev#egg=seneca"
+    ],
     extras_require={
         'dev': open('dev-requirements.txt').readlines()
     },
     entry_points={
         'console_scripts': [
             'storage=cilantro.networking.storage:serve',
-            'witness=cilantro.networking.witness:serve'
+            'witness=cilantro.networking.witness:serve',
+            'cil=cilantro.cli:main'
         ],
     },
     zip_safe=False,
@@ -24,7 +50,7 @@ setup(
     long_description=open('README.md').read(),
     url='https://github.com/Lamden/cilantro',
     author='Lamden',
-    email='team@lamden.io',
+    author_email='team@lamden.io',
     classifiers=[
         'Programming Language :: Python :: 3.6',
     ],
