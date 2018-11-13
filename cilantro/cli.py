e0d2eb16a9cd9033d00f7eb8a8b61d0f8fe52541
Stuart Farmer <stuart@lamden.io>
2018-11-13 13:08:03 -0800

    config stubbed

--- cilantro/cli.py
+++ cilantro/cli.py
@@ -7,8 +7,16 @@ def main():
 
 
 @main.command('config', short_help='Adjust the default directory and network configuration.')
-def config():
-    print('yo')
+@click.option('-i', '--info', is_flag=True)
+@click.option('-d', '--directory', 'directory')
+@click.option('-n', '--network', 'network')
+def config(info, directory, network):
+    if info:
+        print('printin')
+    elif directory:
+        print(directory)
+    elif network:
+        print(network)
 
 if __name__ == '__main__':
     print('yo2')
