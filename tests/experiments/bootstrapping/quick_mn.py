0a3064c398bf6890ed092d780484e214ed37c0ac
Stuart Farmer <stuart@lamden.io>
2018-11-14 13:07:58 -0800

    env var

--- tests/experiments/bootstrapping/quick_mn.py
+++ tests/experiments/bootstrapping/quick_mn.py
@@ -1,6 +1,3 @@
-from cilantro.utils.test.testnet_config import set_testnet_config
-set_testnet_config('1-0-0.json')
-
 from cilantro.constants.testnet import TESTNET_MASTERNODES
 from cilantro.nodes.factory import NodeFactory
 from cilantro.constants.overlay_network import HOST_IP
