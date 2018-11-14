7ceae2f48f71870b59115277b6532505f88227ee
Stuart Farmer <stuart@lamden.io>
2018-11-14 15:18:42 -0800

    stubbed routes

--- cilantro/nodes/delegate/webserver.py
+++ cilantro/nodes/delegate/webserver.py
@@ -7,3 +7,18 @@ app = Sanic(__name__)
 @app.route("/", methods=["GET"])
 async def ping():
     return text('pong')
+
+
+@app.route('/balance', methods=['GET'])
+async def balance(request):
+    return text('pong')
+
+
+@app.route('/contract', methods=['GET'])
+async def contract(request):
+    return text('pong')
+
+
+@app.route('/state', methods=['GET'])
+async def state(request):
+    return text(request)
