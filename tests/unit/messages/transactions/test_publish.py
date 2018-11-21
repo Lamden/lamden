d0b6d4f18298b3c0bdab8218e2357cf513c1e89b
Davis Haba <davis.haba76@gmail.com>
2018-11-20 17:54:31 -0800

    spacing

--- tests/unit/messages/transactions/test_publish.py
+++ tests/unit/messages/transactions/test_publish.py
@@ -15,7 +15,7 @@ class TestPublishTransaction(TestCase):
         nonce = 'ABCD' * 16
 
         tx = PublishTransaction.create(sender_sk=sk, gas_supplied=gas, contract_name=contract_name,
-                                      contract_code=contract_code, nonce=nonce)
+                                       contract_code=contract_code, nonce=nonce)
 
         self.assertEquals(tx.sender, vk)
         self.assertEquals(tx.gas_supplied, gas)
@@ -32,7 +32,7 @@ class TestPublishTransaction(TestCase):
         nonce = 'ABCD' * 16
 
         tx = PublishTransaction.create(sender_sk=sk, gas_supplied=gas, contract_name=contract_name,
-                                                 contract_code=contract_code, nonce=nonce)
+                                       contract_code=contract_code, nonce=nonce)
         clone = PublishTransaction.from_bytes(tx.serialize())
 
         self.assertEqual(clone, tx)
