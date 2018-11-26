07636b35fe1a38f74033ed94582e7f70b3313e24
wywfalcon <wywfalcon@gmail.com>
2018-11-26 12:47:32 -0800

    try this

--- scripts/start_mongo.sh
+++ scripts/start_mongo.sh
@@ -20,7 +20,9 @@ echo 'Dir created'
 
 python3 ./scripts/create_user.py &
 
-if [ "$CIRCLECI" != "true" ]
+if [ "$CIRCLECI" == "true" ]
 then
+  sudo service mongod --dbpath ./data/$HOST_NAME --logpath ./data/$HOST_NAME/logs/mongo.log --bind_ip_all
+else
   mongod --dbpath ./data/$HOST_NAME --logpath ./data/$HOST_NAME/logs/mongo.log --bind_ip_all
 fi
