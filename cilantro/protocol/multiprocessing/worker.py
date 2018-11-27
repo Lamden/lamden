5399fd8bc0f8721505bcbf338dee79a7bfab70bc
Davis Haba <davis.haba76@gmail.com>
2018-11-26 16:20:29 -0800

    Worker classes have ability to pass in socket manager

--- cilantro/protocol/multiprocessing/worker.py
+++ cilantro/protocol/multiprocessing/worker.py
@@ -12,12 +12,17 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 class Worker:
 
-    def __init__(self, signing_key, loop=None, context=None, name=''):
+    def __init__(self, signing_key=None, loop=None, context=None, manager: SocketManager=None, name=''):
         name = name or type(self).__name__
         self.log = get_logger(name)
 
-        # Create a new event loop for this process
-        if context: assert loop, 'If passing context, must also include loop'
+        if manager:
+            assert not loop and not context and not signing_key, "If passing in a SocketManager you should omit all other args"
+            signing_key, context, loop = manager.signing_key, manager.context, manager.loop
+        else:
+            assert signing_key, "Signing key arg is required if not passing in a SocketManager"
+            if context: assert loop, 'If passing context, must also include loop'
+
         self.loop = loop or asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
         self.context = context or zmq.asyncio.Context()
@@ -25,4 +30,4 @@ class Worker:
         self.signing_key = signing_key
         self.verifying_key = wallet.get_vk(self.signing_key)
 
-        self.manager = SocketManager(signing_key=signing_key, context=self.context, loop=self.loop)
+        self.manager = manager or SocketManager(signing_key=signing_key, context=self.context, loop=self.loop)
