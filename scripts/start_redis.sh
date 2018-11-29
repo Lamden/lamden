1c98b9d75f535a38621c6667acb2c8a139c7ccb5
wywfalcon <wywfalcon@gmail.com>
2018-11-28 17:10:32 -0800

    fix it

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -14,7 +14,4 @@ REDIS_PASSWORD=$REDIS_PASSWORD
 
 echo "Starting Redis server..."
 pkill -9 redis-server
-redis-server docker/redis.conf --port $REDIS_PORT --requirepass $REDIS_PASSWORD 2>/dev/null >/dev/null &
-redis-server 2>/dev/null >/dev/null &
-sleep 1
-echo "Done."
+redis-server
