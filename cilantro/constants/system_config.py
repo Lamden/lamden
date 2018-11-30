a2b691d7b6e27b4369c2bb227cc8018e12196c39
Davis Haba <davis.haba76@gmail.com>
2018-11-30 10:49:42 -0800

    clean up system config. default testnet config is now 2-2-4 (i think the set_testnet_config is not working relibley on CI. i changed the write flag from w to w+ in set_testnet_config)

--- cilantro/constants/system_config.py
+++ cilantro/constants/system_config.py
@@ -54,13 +54,4 @@ MIN_NEW_BLOCK_MN_QOURUM = math.ceil(NUM_MASTERS * 2 / 3)  # Number of NewBlockNo
 # ///////////////////////////////////////////////
 # Seneca Interpreter
 # ///////////////////////////////////////////////
-# If MOCK_INTERPRET_RANDOM_MODE=False, we use MOCK_INTERPRET_TIME
-MOCK_INTERPRET_TIME = 0.05
-
-# If MOCK_INTERPRET_RANDOM_MODE=True, we use a random value between MIN_MOCK_INTERPRET_TIME and MAX_MOCK_INTERPRET_TIME
-MOCK_INTERPRET_RANDOM_MODE = True
-MIN_MOCK_INTERPRET_TIME = 0.01
-MAX_MOCK_INTERPRET_TIME = 0.1
-
-# This level of decimal precision for fixed point
 DECIMAL_PRECISION = 18
