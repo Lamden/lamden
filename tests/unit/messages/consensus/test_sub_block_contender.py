410ce0a4753dc4be1302b8f05dfc3dd9403ad8a8
Raghu Chalasani <raghupc@hotmail.com>
2018-11-30 16:14:54 -0800

    new test added

--- tests/unit/messages/consensus/test_sub_block_contender.py
+++ tests/unit/messages/consensus/test_sub_block_contender.py
@@ -1,3 +1,5 @@
+from cilantro.messages.base.base import MessageBase
+from cilantro.messages.envelope.envelope import Envelope
 from cilantro.messages.consensus.sub_block_contender import SubBlockContender, SubBlockContenderBuilder
 from cilantro.messages.consensus.merkle_signature import MerkleSignature, build_test_merkle_sig
 from cilantro.messages.transaction.data import TransactionDataBuilder
@@ -58,6 +60,42 @@ class TestSubBlockContender(TestCase):
         sbc = SubBlockContender.create_empty_sublock(input_hash=input_hash, signature=signature, sub_block_index=0)
 
         self.assertTrue(sbc.is_empty)
+
+    def test_sbc_handshake(self):
+        txs = [TransactionDataBuilder.create_random_tx() for i in range(5)]
+        raw_txs = [tx.serialize() for tx in txs]
+        tree = MerkleTree.from_raw_transactions(raw_txs)
+
+        input_hash = 'B' * 64  # in reality this would be the env hash. we can just make something up
+        signature = build_test_merkle_sig(msg=tree.root, sk=DEL_SK, vk=DEL_VK)
+
+        sbc1 = SubBlockContender.create(result_hash=tree.root_as_hex, input_hash=input_hash, merkle_leaves=tree.leaves,
+                                       signature=signature, transactions=txs, sub_block_index=0)
+        self.assertFalse(sbc1.is_empty)
+        msg_type = MessageBase.registry[type(sbc1)]
+        sbc2 = MessageBase.registry[msg_type].from_bytes(sbc1.serialize())
+        self.assertEqual(sbc1, sbc2)
+        msg2 = Envelope.create_from_message(message=sbc2, signing_key=DEL_SK, verifying_key=DEL_VK)
+        es = msg2.serialize()
+        env = Envelope.from_bytes(es)
+        sbc = env.message
+        if not sbc.signature.verify(bytes.fromhex(sbc.result_hash)):
+            print("This SubBlockContender does not have a correct signature!")
+        if len(sbc.merkle_leaves) > 0:
+            print("This SubBlockContender have num of merkle leaves!")
+            print(len(sbc.merkle_leaves))
+            if MerkleTree.verify_tree_from_bytes(sbc.merkle_leaves, root=sbc.result_hash):
+                for tx in sbc.transactions:
+                    if not tx.hash in sbc.merkle_leaves:
+                        print('Received malicious transactions that does not match any merkle leaves!')
+                        break
+                print('This SubBlockContender is valid!')
+            else:
+                print('This SubblockContender is INVALID!')
+        else:
+            print('This SubBlockContender is empty.')
+
+
 
 
 if __name__ == '__main__':
