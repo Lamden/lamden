469e26434842c22de6839781fab02f47dac4096d
wywfalcon <wywfalcon@gmail.com>
2018-12-10 15:53:58 -0800

    it needs printing anyways

--- tests/experiments/cloud/test_aws.py
+++ tests/experiments/cloud/test_aws.py
@@ -13,23 +13,23 @@ def masternode(idx):
     from cilantro.protocol.overlay.auth import Auth
     import asyncio, os, ujson as json, sys
     from cilantro.storage.vkbook import VKBook
+    log.critical('log something son')
     VKBook.setup(CONSTITUION_JSON)
 
-    async def check_nodes():
-        while True:
-            await asyncio.sleep(1)
-            if len(Discovery.discovered_nodes) >= 1:
-                quit()
-
-    loop = asyncio.get_event_loop()
-    Auth.setup(VKBook.constitution['masternodes'][idx]['sk'])
-    Discovery.setup()
-    tasks = asyncio.ensure_future(asyncio.gather(
-        Discovery.listen(),
-        Discovery.discover_nodes(os.getenv('HOST_IP')),
-        check_nodes()
-    ))
-    log.critical('log something son')
+    # async def check_nodes():
+    #     while True:
+    #         await asyncio.sleep(1)
+    #         if len(Discovery.discovered_nodes) >= 1:
+    #             quit()
+    #
+    # loop = asyncio.get_event_loop()
+    # Auth.setup(VKBook.constitution['masternodes'][idx]['sk'])
+    # Discovery.setup()
+    # tasks = asyncio.ensure_future(asyncio.gather(
+    #     Discovery.listen(),
+    #     Discovery.discover_nodes(os.getenv('HOST_IP')),
+    #     check_nodes()
+    # ))
     # loop.run_until_complete(tasks)
 
 
@@ -41,23 +41,23 @@ def delegates(idx):
     from cilantro.constants.overlay_network import MIN_BOOTSTRAP_NODES
     import asyncio, os, ujson as json, sys
     from cilantro.storage.vkbook import VKBook
+    log.critical('log something daughter')
     VKBook.setup(CONSTITUION_JSON)
 
-    async def check_nodes():
-        while True:
-            await asyncio.sleep(1)
-            if len(Discovery.discovered_nodes) >= MIN_BOOTSTRAP_NODES:
-                quit()
-
-    loop = asyncio.get_event_loop()
-    Auth.setup(VKBook.constitution['delegates'][idx]['sk'])
-    Discovery.setup()
-    tasks = asyncio.ensure_future(asyncio.gather(
-        Discovery.listen(),
-        Discovery.discover_nodes(os.getenv('HOST_IP')),
-        check_nodes()
-    ))
-    log.critical('log something daughter')
+    # async def check_nodes():
+    #     while True:
+    #         await asyncio.sleep(1)
+    #         if len(Discovery.discovered_nodes) >= MIN_BOOTSTRAP_NODES:
+    #             quit()
+    #
+    # loop = asyncio.get_event_loop()
+    # Auth.setup(VKBook.constitution['delegates'][idx]['sk'])
+    # Discovery.setup()
+    # tasks = asyncio.ensure_future(asyncio.gather(
+    #     Discovery.listen(),
+    #     Discovery.discover_nodes(os.getenv('HOST_IP')),
+    #     check_nodes()
+    # ))
     # loop.run_until_complete(tasks)
 
 class TestCloud(AWSTestCase):
