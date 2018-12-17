1738db792a405d97a789e7d1b8adbc41a15ed88b
wywfalcon <wywfalcon@gmail.com>
2018-12-17 11:59:09 -0800

    small change to specify 5-5-5 usecase

--- cilantro/nodes/base.py
+++ cilantro/nodes/base.py
@@ -46,7 +46,6 @@ class NodeBase(Worker):
         self.log = get_logger(name)
         self.ip = ip
         self.name = name
-        self.log.important(self.REQ_MNS)
 
         self.loop = loop or asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
