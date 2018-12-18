780c387ee46230c4ba5c7663905072f4c7689566
Davis Haba <davis.haba76@gmail.com>
2018-12-18 10:54:13 -0800

    tests for catchup manager

--- tests/unit/nodes/test_catchup_manager.py
+++ tests/unit/nodes/test_catchup_manager.py
@@ -18,13 +18,18 @@ class TestCatchupManager(TestCase):
 
     def setUp(self):
         SafeRedis.flushall()
+        self.manager = None
 
     def tearDown(self):
+        if self.manager.timeout_fut and not self.manager.timeout_fut.done():
+            self.manager.timeout_fut.cancel()
         asyncio.get_event_loop().close()
 
     def _build_manager(self, vk='A'*64, store_blocks=True) -> CatchupManager:
         pub, router = MagicMock(), MagicMock()
-        return CatchupManager(verifying_key=vk, pub_socket=pub, router_socket=router, store_full_blocks=store_blocks)
+        m = CatchupManager(verifying_key=vk, pub_socket=pub, router_socket=router, store_full_blocks=store_blocks)
+        self.manager = m
+        return m
 
     def test_init(self):
         m = self._build_manager()
@@ -32,7 +37,6 @@ class TestCatchupManager(TestCase):
         self.assertEqual(m.curr_hash, StateDriver.get_latest_block_hash())
         self.assertEqual(m.curr_num, StateDriver.get_latest_block_num())
 
-    # TODO fix this
     # def test_catchup_with_no_new_blocks(self):
     #     cm = self._build_manager()
     #
@@ -49,7 +53,47 @@ class TestCatchupManager(TestCase):
     #
     #     self.assertFalse(cm.catchup_state)
 
+    def test_catchup_with_new_blocks(self):
+        cm = self._build_manager()
+        cm.run_catchup()
+        self.assertTrue(cm.catchup_state)
 
+        b1 = 'A' * 64
+        b2 = 'B' * 64
+        vk1 = VKBook.get_masternodes()[0]
+        vk2 = VKBook.get_masternodes()[1]
+
+        reply_data1 = [{'blockHash': b1, 'blockOwners': [vk1, vk2], 'blockNum': 1},
+                       {'blockHash': b2, 'blockOwners': [vk1, vk2], 'blockNum': 2}]
+        reply_data2 = [{'blockHash': b1, 'blockOwners': [vk1, vk2], 'blockNum': 1},
+                       {'blockHash': b2, 'blockOwners': [vk1, vk2], 'blockNum': 2}]
+
+        index_reply1 = BlockIndexReply.create(reply_data1)
+        index_reply2 = BlockIndexReply.create(reply_data2)
+
+        cm.recv_block_idx_reply(vk1, index_reply1)
+        self.assertFalse(cm.catchup_state)
+
+        cm.recv_block_idx_reply(vk2, index_reply1)
+        self.assertTrue(cm.catchup_state)
+
+    def test_catchup_with_no_new_blocks(self):
+        cm = self._build_manager()
+
+        cm.run_catchup()
+
+        self.assertTrue(cm.catchup_state)
+
+        reply_data = []
+        index_reply = BlockIndexReply.create(reply_data)
+
+        # We should still be in catchup until we achieve consensus on IndexReplies
+        cm.recv_block_idx_reply(VKBook.get_masternodes()[0], index_reply)
+        self.assertTrue(cm.catchup_state)
+
+        # Now that we have 2/2 replies, we should be out of Catchup
+        cm.recv_block_idx_reply(VKBook.get_masternodes()[1], index_reply)
+        self.assertFalse(cm.catchup_state)
 
 """
   9 block index request:
