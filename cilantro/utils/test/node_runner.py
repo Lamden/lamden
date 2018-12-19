287ac20749a8f6bc28f05e08703300658211586f
Davis Haba <davis.haba76@gmail.com>
2018-12-19 13:07:46 -0800

    refactor test code

---
+++ cilantro/utils/test/node_runner.py
@@ -0,0 +1,92 @@
+def wrap_func(fn, *args, **kwargs):
+    def wrapper():
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def log_create(name, vk, ip):
+    from cilantro.logger import get_logger
+    log = get_logger("{}Builder".format(name))
+    delim = '=' * 64
+    msg = '\n' + delim + '\n' + 'Creating {} with:\nvk={}\nip={}\n'.format(name, vk, ip) + delim
+    log.test(msg)
+
+
+def run_mn(slot_num, log_lvl=11, reset_db=False, nonce_enabled=True):
+    # Due to some Sanic BS, we cannot set the log level between [1,9]
+    assert log_lvl not in range(1, 10), "Due to a Sanic logging bug, Masternode cant set log lvl in the range [1,10]"
+
+    import os
+    if not nonce_enabled:
+        # We must set this env var before we import anything from cilantro
+        os.environ["NONCE_DISABLED"] = "1"
+
+    from cilantro.logger import get_logger, overwrite_logger_level
+    from cilantro.nodes.factory import NodeFactory
+    from cilantro.constants.testnet import TESTNET_MASTERNODES
+    from cilantro.utils.test.node_runner import log_create
+
+    overwrite_logger_level(log_lvl)
+
+    ip = os.getenv('HOST_IP')
+    vk, sk = TESTNET_MASTERNODES[slot_num]['vk'],  TESTNET_MASTERNODES[slot_num]['sk']
+    log_create("Masternode", vk, ip)
+    NodeFactory.run_masternode(ip=ip, signing_key=sk, reset_db=reset_db)
+
+
+def run_witness(slot_num, log_lvl=11, reset_db=False, return_fn=True):
+    from cilantro.logger import get_logger, overwrite_logger_level
+    from cilantro.nodes.factory import NodeFactory
+    from cilantro.constants.testnet import TESTNET_WITNESSES
+    import os
+    from cilantro.utils.test.node_runner import log_create
+
+    overwrite_logger_level(log_lvl)
+
+    vk, sk = TESTNET_WITNESSES[slot_num]['vk'],  TESTNET_WITNESSES[slot_num]['sk']
+    ip = os.getenv('HOST_IP')
+    log_create("Witness", vk, ip)
+    NodeFactory.run_witness(ip=ip, signing_key=sk, reset_db=reset_db)
+
+
+def run_delegate(slot_num, log_lvl=11, seneca_log_lvl=11, reset_db=False, return_fn=True):
+    from cilantro.logger import get_logger, overwrite_logger_level
+    from seneca.libs.logger import overwrite_logger_level as sen_overwrite_log
+    from cilantro.nodes.factory import NodeFactory
+    from cilantro.constants.testnet import TESTNET_DELEGATES
+    from cilantro.utils.test.node_runner import log_create
+    import os
+
+    overwrite_logger_level(log_lvl)
+    sen_overwrite_log(seneca_log_lvl)
+
+    vk, sk = TESTNET_DELEGATES[slot_num]['vk'],  TESTNET_DELEGATES[slot_num]['sk']
+    ip = os.getenv('HOST_IP')
+    log_create("Delegate", vk, ip)
+    NodeFactory.run_delegate(ip=ip, signing_key=sk, reset_db=reset_db)
+
+
+def dump_it(volume, delay=0):
+    from cilantro.utils.test.god import God
+    from cilantro.logger import get_logger, overwrite_logger_level
+    import logging
+
+    overwrite_logger_level(logging.WARNING)
+    God._dump_it(volume=volume, delay=delay)
+
+
+def pump_it(lamd, use_poisson=True, pump_wait=0):
+    from cilantro.utils.test.god import God
+    from cilantro.logger import get_logger, overwrite_logger_level
+    import logging, time
+
+    overwrite_logger_level(logging.WARNING)
+
+    log = get_logger("Pumper")
+
+    if pump_wait > 0:
+        log.important("Pumper sleeping {} seconds before starting...".format(pump_wait))
+        time.sleep(pump_wait)
+
+    log.important("Starting the pump..")
+    God._pump_it(rate=lamd, use_poisson=use_poisson)
--- tests/cloud/test_pump.py
+++ tests/cloud/test_pump.py
@@ -1,5 +1,12 @@
+NETWORK_SIZE = '4-4-4'
+
+MN_LOG_LVL = 11
+WITNESS_LOG_LVL = 11
+DELEGATE_LOG_LVL = 11
+SENECA_LOG_LVL = 11
+
 from cilantro.utils.test.testnet_config import set_testnet_config
-set_testnet_config('4-4-4.json')
+set_testnet_config('{}.json'.format(NETWORK_SIZE))
 
 from cilantro.constants.vmnet import get_config_file
 from vmnet.cloud.testcase import AWSTestCase
@@ -88,41 +95,51 @@ def pump_it(lamd, use_poisson, pump_wait):
     time.sleep(pump_wait)
 
     log.important("Starting the pump..")
-    God.pump_it(rate=lamd, use_poisson=use_poisson)
+    God._pump_it(rate=lamd, use_poisson=use_poisson)
 
 
 class TestPump(AWSTestCase):
 
     NUM_BLOCKS = 2
     VOLUME = TRANSACTIONS_PER_SUB_BLOCK * NUM_SB_PER_BLOCK * NUM_BLOCKS  # Number of transactions to dum
-    config_file = get_config_file('cilantro-aws-4-4-4.json')
+    config_file = get_config_file('cilantro-aws-{}.json'.format(NETWORK_SIZE))
     keep_up = True
     logging = True
 
     # Avg number of transactions per second we will pump. Set to pump 1 block per BATCH_SLEEP_INTERVAL
     PUMP_RATE = (TRANSACTIONS_PER_SUB_BLOCK * NUM_SB_PER_BLOCK) // BATCH_SLEEP_INTERVAL
     MODEL_AS_POISSON = True
-    PUMP_WAIT = 120  # how long to sleep before we start the pump
+    PUMP_WAIT = 180  # how long to sleep before we start the pump
 
     def test_pump(self):
         log = get_logger("Pumpatron")
 
         # Bootstrap master
         for i, nodename in enumerate(self.groups['masternode']):
-            self.execute_python(nodename, wrap_func(run_mn, i))
+            self.execute_python(nodename, God.run_mn(i, log_lvl=MN_LOG_LVL, nonce_enabled=False, reset_db=True))
 
         # Bootstrap witnesses
         for i, nodename in enumerate(self.groups['witness']):
-            self.execute_python(nodename, wrap_func(run_witness, i))
+            self.execute_python(nodename, God.run_witness(i, log_lvl=WITNESS_LOG_LVL, reset_db=True))
 
         # Bootstrap delegates
         for i, nodename in enumerate(self.groups['delegate']):
-            self.execute_python(nodename, wrap_func(run_delegate, i))
+            self.execute_python(nodename, God.run_delegate(i, log_lvl=DELEGATE_LOG_LVL, seneca_log_lvl=SENECA_LOG_LVL,
+                                                           reset_db=True))
 
         # Bootstrap pump
-        self.execute_python('mgmt', wrap_func(pump_it, self.PUMP_RATE, self.MODEL_AS_POISSON, self.PUMP_WAIT))
+        self.execute_python('mgmt', God.pump_it(self.PUMP_RATE, self.MODEL_AS_POISSON, self.PUMP_WAIT))
 
-        # TODO also allow user to dump also??
+        while True:
+            user_input = input("Enter an integer representing the # of transactions to dump, or 'x' to quit.")
+
+            if user_input.lower() == 'x':
+                log.important("Termination input detected. Breaking")
+                break
+
+            vol = int(user_input) if user_input.isdigit() else self.VOLUME
+            log.important3("Dumpatron dumping {} transactions!".format(vol))
+            self.execute_python('mgmt', God.dump_it(volume=vol))
 
 
 if __name__ == '__main__':
