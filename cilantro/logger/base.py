4bd43f207236f1836d2c25a4a1bdd17b92eb8579
wywfalcon <wywfalcon@gmail.com>
2018-12-20 11:15:30 -0800

    include file

--- cilantro/logger/base.py
+++ cilantro/logger/base.py
@@ -153,7 +153,7 @@ def get_logger(name=''):
     ]
 
     if os.getenv('VMNET_CLOUD'):
-        filehandlers.append(S3Handler())
+        filehandlers.append(S3Handler(filename))
 
     logging.basicConfig(
         format=format,
