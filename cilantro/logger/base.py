5505dfd32fb9d74e53f15107aee533b20370401a
wywfalcon <wywfalcon@gmail.com>
2018-12-20 15:20:09 -0800

    typhoe

--- cilantro/logger/base.py
+++ cilantro/logger/base.py
@@ -154,11 +154,7 @@ def get_logger(name=''):
     if os.getenv('VMNET_CLOUD'):
         if not os.getenv('LOGGER_ENABLED'):
             os.environ['LOGGER_ENABLED'] = 'True'
-            filehandlers.append(
-                AWSCloudWatchHandler(
-                    os.getenv('TEST_NAME'),
-                    os.getenv('HOST_NAME', name)
-                ))
+            filehandlers.append(AWSCloudWatchHandler())
 
     logging.basicConfig(
         format=format,
