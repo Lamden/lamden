a846b64744ec843a6d61ddf944f6652dc846b2e3
wywfalcon <wywfalcon@gmail.com>
2018-12-19 17:24:03 -0800

    get rid of vvvvv

--- cilantro/logger/base.py
+++ cilantro/logger/base.py
@@ -153,7 +153,6 @@ def get_logger(name=''):
     ]
 
     if os.getenv('VMNET_CLOUD'):
-        print('vvvvv: {}'.format(os.getenv('VMNET_CLOUD')))
         filehandlers.append(S3Handler())
 
     logging.basicConfig(
