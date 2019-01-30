d6880cc5aabf5aaaee67d20ae0b5a63289791dfc
Davis Haba <davis.haba76@gmail.com>
2019-01-30 11:23:18 -0800

    WIP on config generator...unit tests passing

--- cilantro/utils/test/testnet_config.py
+++ cilantro/utils/test/testnet_config.py
@@ -5,7 +5,6 @@ import cilantro
 import configparser
 from cilantro.protocol import wallet
 from cilantro.logger.base import get_logger
-from cilantro.constants.vmnet import test_dir
 import json, os
 from os.path import join
 
@@ -23,7 +22,8 @@ log = get_logger("TestnetNodeBuilder")
 os.environ['__INHERIT_CONSTITUTION__'] = 'True'
 
 DEFAULT_TESTNET_FILE_NAME = '4-4-4.json'
-TESTNET_JSON_DIR = test_dir
+TESTNET_JSON_DIR = os.path.dirname(cilantro.__path__[-1]) + '/constitutions/test'
+# print("TESTNET JSON DIR: {}".format(TESTNET_JSON_DIR))
 
 TESTNET_KEY = 'testnet'
 TESTNET_JSON_KEY = 'testnet_json_file_name'
@@ -42,6 +42,11 @@ def set_testnet_config(testnet_json_file='4-4-4.json'):
     set_testnet_nodes()
 
 
+def get_testnet_config():
+    with open(get_testnet_json_path(), 'r') as f:
+        return json.load(f)
+
+
 def get_config_filename():
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE_PATH)
@@ -53,29 +58,3 @@ def get_testnet_json_path():
     config.read(CONFIG_FILE_PATH)
     return join(TESTNET_JSON_DIR, config[TESTNET_KEY][TESTNET_JSON_KEY])
 
-
-def generate_testnet_json(num_masters=NUM_MASTERS, num_witnesses=NUM_WITNESSES, num_delegates=NUM_DELEGATES):
-    def _build_nodes(num_nodes=64, prefix='node') -> list:
-        nodes = []
-
-        for i in range(num_nodes):
-            # name = "{}_{}".format(prefix, i + 1)
-            sk, vk = wallet.new()
-            nodes.append({'sk': sk, 'vk': vk})
-
-        return nodes
-
-    def build_masternodes(num_nodes=64):
-        return _build_nodes(num_nodes=num_nodes, prefix='masternode')
-
-    def build_witnesses(num_nodes=256):
-        return _build_nodes(num_nodes=num_nodes, prefix='witness')
-
-    def build_delegate(num_nodes=32):
-        return _build_nodes(num_nodes=num_nodes, prefix='delegate')
-
-    testnet = {'masternodes': build_masternodes(num_masters), 'witnesses': build_witnesses(num_witnesses),
-               'delegates': build_delegate(num_delegates)}
-
-    with open(TESTNET_JSON_DIR + 'new.json', 'w') as fp:
-        json.dump(testnet, fp, indent=4)
