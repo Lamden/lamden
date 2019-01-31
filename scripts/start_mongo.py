e267d7466d377b42e58032cef231b680c180df6e
Davis Haba <davis.haba76@gmail.com>
2019-01-31 11:35:55 -0800

    wip on circus config

---
+++ scripts/start_mongo.py
@@ -0,0 +1,34 @@
+import os, shutil
+from os import getenv as env
+from create_user import create_user
+
+
+def start_mongo():
+    if env('VMNET_CLOUD'):
+        host_name = ''
+        if env('ANNIHILATE'):
+            shutil.rmtree('./data', ignore_errors=True)
+    else:
+        host_name = env('HOST_NAME', '')
+
+    print("Waiting for mongo on localhost")
+    try:
+        os.makedirs('./data/{}/logs'.format(host_name), exist_ok=True)
+        with open('./data/{}/logs/mongo.log'.format(host_name), 'w+') as f:
+            print('Dir created')
+    except:
+        pass
+
+    print('Starting mongo server...')
+    os.system('sudo pkill -9 mongod')
+    os.system('mongod --dbpath ./data/{} --logpath ./data/{}/logs/mongo.log {} &'.format(
+        host_name, host_name, '' if env('CIRCLECI') == 'true' else '--bind_ip_all'
+    ))
+
+    create_user()
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+    start_mongo()
