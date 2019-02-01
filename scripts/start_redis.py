f556fc2d1a94b42f9a6f42b4d773e2d70d88a2aa
Davis Haba <davis.haba76@gmail.com>
2019-02-01 14:59:09 -0800

    helpme

--- scripts/start_redis.py
+++ scripts/start_redis.py
@@ -2,14 +2,14 @@ import os
 
 
 def start_redis():
-    # if not os.getenv('CIRCLECI') and not os.getenv('VMNET_CLOUD'):
-        # for package in ['seneca', 'vmnet']:
-        #     os.system('cp -r ./venv/lib/python3.6/site-packages/{} /usr/local/lib/python3.6/dist-packages 2>/dev/null'.format(package))
-
+    redis_dir = '/data/db/cilantro'
     print("Starting Redis server...")
-    redis_dir = 'redis-store/{}'.format(os.getenv('HOST_NAME', 'local'))
+
+    if not os.path.exists(redis_dir):
+        print("Creating Redis directory at {}".format(redis_dir))
+        os.makedirs(redis_dir, exist_ok=True)
+
     redis_file = 'dump.rdb'
-    os.makedirs(redis_dir, exist_ok=True)
 
     print("Redis using data directory: {}\nAnd db file name: {}".format(redis_dir, redis_file))
     os.system('redis-server --dir {} --dbfilename {}'.format(redis_dir, redis_file))
