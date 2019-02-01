6395aa5ebedc4d4d9d3e222c49d2c38e74f54c24
Davis Haba <davis.haba76@gmail.com>
2019-02-01 14:00:05 -0800

    wip

--- scripts/start_redis.py
+++ scripts/start_redis.py
@@ -2,20 +2,17 @@ import os
 
 
 def start_redis():
-    if not os.getenv('CIRCLECI') and not os.getenv('VMNET_CLOUD'):
-        for package in ['seneca', 'vmnet']:
-            os.system('cp -r ./venv/lib/python3.6/site-packages/{} /usr/local/lib/python3.6/dist-packages 2>/dev/null'.format(package))
+    # if not os.getenv('CIRCLECI') and not os.getenv('VMNET_CLOUD'):
+        # for package in ['seneca', 'vmnet']:
+        #     os.system('cp -r ./venv/lib/python3.6/site-packages/{} /usr/local/lib/python3.6/dist-packages 2>/dev/null'.format(package))
 
     print("Starting Redis server...")
     redis_dir = 'redis-store/{}'.format(os.getenv('HOST_NAME', 'local'))
     redis_file = 'dump.rdb'
     os.makedirs(redis_dir, exist_ok=True)
 
-    if os.getenv('CIRCLECI'):
-        os.system('redis-server')
-    else:
-        print("Using redis directory: {}\nAnd db file name: {}".format(redis_dir, redis_file))
-        os.system('redis-server --dir {} --dbfilename {}'.format(redis_dir, redis_file))
+    print("Redis using data directory: {}\nAnd db file name: {}".format(redis_dir, redis_file))
+    os.system('redis-server --dir {} --dbfilename {}'.format(redis_dir, redis_file))
 
 
 if __name__ == '__main__':
