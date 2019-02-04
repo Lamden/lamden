d643cb3a596568489c2725b5307bd0f3be677697
Davis Haba <davis.haba76@gmail.com>
2019-02-04 11:28:05 -0800

    yay conf looking good

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -1,33 +1,40 @@
-from cilantro.utils.test.testnet_config import set_testnet_config
-set_testnet_config('1-0-0.json')
+import os, configparser
 
-from cilantro.logger import get_logger, overwrite_logger_level
-from cilantro.nodes.factory import NodeFactory
-from cilantro.constants.testnet import TESTNET_MASTERNODES
+CONFIG_PATH = '/etc/cilantro.conf'
+
+assert os.path.exists(CONFIG_PATH), "No config file found at path {}".format(CONFIG_PATH)
+config = configparser.ConfigParser()
+config.read(CONFIG_PATH)
+config = config['DEFAULT']
+
+from cilantro.utils.test.testnet_config import set_testnet_config
+
+set_testnet_config(config['constitution_file'])
+
 from cilantro.storage.vkbook import VKBook
-import os
-import logging
-import time
+from cilantro.utils.test.node_runner import *
 
 
 def main():
     print("BoOoOoOoOoOTSSSSSSRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPPPP")
 
+    if config['ssl_enabled']:
+        os.environ['SSL_ENABLED'] = "1"
 
-    # overwrite_logger_level(logging.WARNING)
-    # overwrite_logger_level(21)
-    # overwrite_logger_level(11)
+    os.environ['HOST_IP'] = config['ip']
 
-    os.environ["NONCE_DISABLED"] = "1"
-    os.environ["MN_MOCK"] = "1"
+    # print("VKBook mns {}".format(VKBook.get_masternodes()))
 
-
-    print("VKBook mns {}".format(VKBook.get_masternodes()))
-    print("TESTNET MASTERNODES {}".format(TESTNET_MASTERNODES))
-
-    ip = '127.0.0.1'
-    sk = TESTNET_MASTERNODES[0]['sk']
-    NodeFactory.run_masternode(ip=ip, signing_key=sk, reset_db=True)
+    if config['node_type'] == 'delegate':
+        run_witness(sk=config['sk'], log_lvl=config['log_lvl'], reset_db=config['reset_db'])
+    elif config['node_type'] == 'witness':
+        run_delegate(sk=config['sk'], log_lvl=config['log_lvl'], seneca_log_lvl=config['seneca_log_lvl'],
+                     reset_db=config['reset_db'])
+    elif config['node_type'] == 'masternode':
+        run_mn(sk=config['sk'], log_lvl=config['log_lvl'], nonce_enabled=config['nonce_enabled'],
+               reset_db=config['reset_db'])
+    else:
+        raise Exception("Unrecognized node type {}".format(config['node_type']))
 
 
 if __name__ == '__main__':
