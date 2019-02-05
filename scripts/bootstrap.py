cf637d6cab7e19be1a24fb645b84673c5c4fc553
Davis Haba <davis.haba76@gmail.com>
2019-02-05 15:04:35 -0800

    update bootstrap and overlay net discovery timeout

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -1,20 +1,16 @@
 import os, configparser
+from cilantro.utils.test.node_runner import *
 
 CONFIG_PATH = '/etc/cilantro.conf'
-
-assert os.path.exists(CONFIG_PATH), "No config file found at path {}".format(CONFIG_PATH)
-config = configparser.ConfigParser()
-config.read(CONFIG_PATH)
-config = config['DEFAULT']
-
-# from cilantro.utils.test.testnet_config import set_testnet_config
-# set_testnet_config(config['constitution_file'])
-
-from cilantro.utils.test.node_runner import *
 
 
 def main():
     print("BoOoOoOoOoOTSSSSSSRAaaAAAaaaaaaaaaaAAAAAAaaaaaaaaaAAAAAAAAAAaaaaaaaAAAAAaaaaaaaaAAAAAAAAPPpppPP")
+
+    assert os.path.exists(CONFIG_PATH), "No config file found at path {}".format(CONFIG_PATH)
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
+    config = config['DEFAULT']
 
     # Some configs are specified as env vars so they can be accessible across multiple files
     if config['ssl_enabled']:
@@ -26,12 +22,12 @@ def main():
     # print("VKBook mns {}".format(VKBook.get_masternodes()))
 
     if config['node_type'] == 'witness':
-        run_witness(sk=config['sk'], log_lvl=config['log_lvl'], reset_db=config['reset_db'])
+        run_witness(sk=config['sk'], log_lvl=int(config['log_lvl']), reset_db=config['reset_db'])
     elif config['node_type'] == 'delegate':
-        run_delegate(sk=config['sk'], log_lvl=config['log_lvl'], seneca_log_lvl=config['seneca_log_lvl'],
+        run_delegate(sk=config['sk'], log_lvl=int(config['log_lvl']), seneca_log_lvl=int(config['seneca_log_lvl']),
                      reset_db=config['reset_db'])
     elif config['node_type'] == 'masternode':
-        run_mn(sk=config['sk'], log_lvl=config['log_lvl'], nonce_enabled=config['nonce_enabled'],
+        run_mn(sk=config['sk'], log_lvl=int(config['log_lvl']), nonce_enabled=config['nonce_enabled'],
                reset_db=config['reset_db'])
     else:
         raise Exception("Unrecognized node type {}".format(config['node_type']))
