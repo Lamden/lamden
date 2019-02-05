d4c842f1f4432403a8ed5449db7093275d4f5f99
Davis Haba <davis.haba76@gmail.com>
2019-02-05 10:31:13 -0800

    pretty echo

--- ops/tools/docker_build_push.sh
+++ ops/tools/docker_build_push.sh
@@ -22,17 +22,20 @@ echo -e "remoteTag $remoteTag"
 echo -e "\n--------------------------------------------"
 echo -e "Building base image...\n"
 docker build -t lamden/cilantro_base:$localTag -f docker/cilantro_base .
+echo -e "--------------------------------------------"
 
 echo -e "\n--------------------------------------------"
 echo -e "Building light image...\n"
-#docker build  --build-arg BASE=lamden/cilantro_base:$localTag -t lamden/cilantro_light:$localTag -f docker/cilantro_light .
+docker build  --build-arg BASE=lamden/cilantro_base:$localTag -t lamden/cilantro_light:$localTag -f docker/cilantro_light .
+echo -e "--------------------------------------------"
 
 echo -e "\n--------------------------------------------"
 echo -e "Building full image...\n"
 docker build  --build-arg BASE=lamden/cilantro_base:$localTag -t lamden/cilantro_full:$localTag -f docker/cilantro_full .
+echo -e "--------------------------------------------"
 
-docker tag lamden/cilantro_light:$localTag lamden/cilantro_light:$remoteTag
-docker tag lamden/cilantro_full:$localTag lamden/cilantro_full:$remoteTag
+#docker tag lamden/cilantro_light:$localTag lamden/cilantro_light:$remoteTag
+#docker tag lamden/cilantro_full:$localTag lamden/cilantro_full:$remoteTag
 
 if [ "$1" = "--push" ]
 then
@@ -40,3 +43,4 @@ then
     docker push lamden/cilantro_light:$localTag
     docker push lamden/cilantro_full:$localTag
 fi
+
