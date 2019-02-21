f30618e17d5d8e5b484bc9f2071fac538613d6e6
Davis Haba <davis.haba76@gmail.com>
2019-02-20 22:43:05 -0800

    run unit instead of integration silly guy

--- scripts/start_unit_tests.sh
+++ scripts/start_unit_tests.sh
@@ -3,4 +3,4 @@
 #docker run --name cil -dit -v /var/db/cilantro/:/var/db/cilantro -v ~/cilantro/ops/base/circus_unittest.conf:/etc/circus.conf lamden/cilantro_full:$(bash ~/cilantro/ops/tools/generate_tag.sh)
 
 source /app/venv/bin/activate
-python3 /app/tests/run_tests.py --integration 1 --unit 0
+python3 /app/tests/run_tests.py --integration 0 --unit 1
