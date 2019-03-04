0df659e5876d5d5d8024561339e02d9434485338
Davis Haba <davis.haba76@gmail.com>
2019-03-04 14:30:30 -0800

    wip....fixed host_ip issues

--- cilantro_ee/protocol/overlay/kademlia/handshake.py
+++ cilantro_ee/protocol/overlay/kademlia/handshake.py
@@ -6,6 +6,7 @@ from zmq.auth.thread import ThreadAuthenticator
 from zmq.auth.asyncio import AsyncioAuthenticator
 # from cilantro_ee.protocol.overlay.ip import *
 # from cilantro_ee.protocol.comm.socket_auth import SocketAuth
+from cilantro_ee.constants.conf import CilantroConf
 from cilantro_ee.protocol.utils.socket import SocketUtil
 from cilantro_ee.utils.keys import Keys
 from cilantro_ee.logger import get_logger
@@ -19,7 +20,7 @@ class Handshake:
         self.ctx = ctx
 
         self.log = get_logger('OS.Handshake')
-        self.host_ip = HOST_IP
+        self.host_ip = CilantroConf.HOST_IP
         self.port = AUTH_PORT
         self.url = 'tcp://*:{}'.format(self.port)
         self.identity = '{}:{}'.format(self.vk, self.host_ip)
