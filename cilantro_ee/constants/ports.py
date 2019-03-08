76e09271d546c1b18be7c62640184bdedccdaca8
Colin McGrath <cpmcgrat@gmail.com>
2019-03-08 15:47:03 -0800

    updated ports, cleaned envs

--- cilantro_ee/constants/ports.py
+++ cilantro_ee/constants/ports.py
@@ -1,24 +1,24 @@
 # Webservers
-WEB_SERVER_PORT = 8080
-SSL_WEB_SERVER_PORT = 443
+WEB_SERVER_PORT = 8080 # Used to serve HTTP traffic
+SSL_WEB_SERVER_PORT = 443 # Used to serve HTTPS traffic
 
 # ---------------------------------------------------------------
-# Overlay Ports
+# Overlay Ports :10000-10009
 # ---------------------------------------------------------------
-DISCOVERY_PORT = 10004
-AUTH_PORT = 10005
-DHT_PORT = 10006
+DISCOVERY_PORT = 10000
+AUTH_PORT = 10001
+DHT_PORT = 10002
 
 # ---------------------------------------------------------------
-# Masternode Ports
+# Masternode Ports :10010-10019
 # ---------------------------------------------------------------
-MN_ROUTER_PORT = 10008
-MN_PUB_PORT = 10009
-MN_TX_PUB_PORT = 10011
+MN_ROUTER_PORT = 10010
+MN_PUB_PORT = 10011
+MN_TX_PUB_PORT = 10012
 
 # ---------------------------------------------------------------
-# Delegate Ports
+# Delegate Ports :10020-10029
 # ---------------------------------------------------------------
-DELEGATE_ROUTER_PORT = 10016
-DELEGATE_PUB_PORT = 10017
-SBB_PORT_START = 10018   # only in use when witnesses are present in system (not in use in cilantro enterprise)
+DELEGATE_ROUTER_PORT = 10020
+DELEGATE_PUB_PORT = 10021
+SBB_PORT_START = 10022   # only in use when witnesses are present in system (not in use in cilantro enterprise)
