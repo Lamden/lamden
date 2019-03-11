eab6c9365e9fa05660945c2a01a71d0aca0fa5dd
wywfalcon <wywfalcon@gmail.com>
2019-03-11 12:37:38 -0700

    change to ledis

--- scripts/start_redis.py
+++ scripts/start_redis.py
@@ -2,23 +2,23 @@ import os, sys
 from cilantro_ee.constants.db_config import DATA_DIR
 
 
-REDIS_CONF_PATH = '/etc/redis.conf'
-REDIS_DIR = DATA_DIR + '/redis'
+LEDIS_CONF_PATH = '/etc/ledis.conf'
+LEDIS_DIR = DATA_DIR + '/redis'
 
 
 def start_redis(conf_path):
     print("Starting Redis server...")
     if not os.path.exists(REDIS_DIR):
-        print("Creating Redis directory at {}".format(REDIS_DIR))
+        print("Creating Redis directory at {}".format(LEDIS_DIR))
         os.makedirs(REDIS_DIR, exist_ok=True)
 
-    print("Redis using data directory: {}".format(REDIS_DIR))
+    print("Redis using data directory: {}".format(LEDIS_DIR))
 
     if conf_path is not None:
         assert os.path.exists(conf_path), "No redis.conf file found at path {}".format(conf_path)
-        os.system('redis-server {}'.format(conf_path))
+        os.system('ledis-server -config={} {}'.format(LEDIS_CONF_PATH, conf_path))
     else:
-        os.system('redis-server')
+        os.system('ledis-server')
 
 
 if __name__ == '__main__':
@@ -28,5 +28,5 @@ if __name__ == '__main__':
         else:
             start_redis(sys.argv[1])
     else:
-        start_redis(REDIS_CONF_PATH)
+        start_redis(LEDIS_CONF_PATH)
 
