b0f94733535e1f2ad1a3214e173c9fc1627c0003
Stuart Farmer <stuart@lamden.io>
2019-05-10 11:41:56 -0700

    filename function works

--- tests/unit/test_contract_sync.py
+++ tests/unit/test_contract_sync.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
 from cilantro_ee.contracts import sync
 
-class TestContract_sync(TestCase):
+
+class TestContractSync(TestCase):
     def test_directory_to_filename_works(self):
         directory = '~/something/something/hello/this/is/a/path.txt'
-
         name = 'path'
-
         _name = sync.contract_name_from_file_path(directory)
 
         self.assertEqual(name, _name)
+
+    def test_directory_to_filename_if_just_filename(self):
+        directory = 'path.txt'
+        name = 'path'
+        _name = sync.contract_name_from_file_path(directory)
+
+        self.assertEqual(name, _name)
+
+    def test_directory_to_filename_if_many_extensions(self):
+        directory = 'path.txt.a.s.g.we.2.d.g.a.s.c.g'
+        name = 'path'
+        _name = sync.contract_name_from_file_path(directory)
+
+        self.assertEqual(name, _name)
+
