eb180b5b2aff87f380a70a5a0b3a008baffc32db
Stuart Farmer <stuart@lamden.io>
2019-05-21 14:44:46 -0700

    client instead of direct driver (should solve issues)

--- cilantro_ee/contracts/sync.py
+++ cilantro_ee/contracts/sync.py
@@ -1,7 +1,7 @@
 import glob
 import os
-from contracting.db.driver import ContractDriver
-from contracting.compilation.compiler import ContractingCompiler
+from contracting.client import ContractingClient
+
 
 def contract_name_from_file_path(p: str) -> str:
     directories = p.split('/')
@@ -19,33 +19,22 @@ def contracts_for_directory(path, extension):
     return contracts
 
 
-def submit_files(contracts, _compile=True, lint=True, author='sys'):
-    compiler = ContractingCompiler()
-    driver = ContractDriver()
-
-    for contract in contracts:
-        name = contract_name_from_file_path(contract)
-
-        if driver.get_contract(name) is None:
-            with open(contract) as f:
-                contract = f.read()
-
-            if _compile:
-                contract = compiler.parse_to_code(contract, lint=lint)
-
-            driver.set_contract(name=name,
-                           code=contract,
-                           author=author)
-            driver.commit()
-
-
 def sync_genesis_contracts(genesis_path: str='genesis',
-                           direct_path: str='direct',
                            extension: str='*.s.py'):
 
     # Direct database writing of all contract files in the 'genesis' folder
-    direct_contracts = contracts_for_directory(direct_path, extension)
-    submit_files(direct_contracts, _compile=False)
+    # direct_contracts = contracts_for_directory(direct_path, extension)
+    # explicitly submit the submission contract
+    submission_file = __file__ + '/submission.s.py'
+    client = ContractingClient(submission_filename=submission_file)
 
     genesis_contracts = contracts_for_directory(genesis_path, extension)
-    submit_files(genesis_contracts)
+
+    for contract in genesis_contracts:
+        name = contract_name_from_file_path(contract)
+
+        if client.get_contract(name) is None:
+            with open(contract) as f:
+                code = f.read()
+
+            client.submit(code, name=name)
