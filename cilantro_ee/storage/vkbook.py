0b46d94788375604f2f5300204d4c4996accd663
Stuart Farmer <stuart@lamden.io>
2019-06-11 17:44:51 -0700

    no sync either

--- cilantro_ee/storage/vkbook.py
+++ cilantro_ee/storage/vkbook.py
@@ -46,9 +46,9 @@ class VKBook(metaclass=VKBookMeta):
         from cilantro_ee.utils.test.testnet_config import read_public_constitution
 
         #from contracting.client import ContractingClient
-        from cilantro_ee.contracts import sync
+        #from cilantro_ee.contracts import sync
 
-        sync.sync_genesis_contracts()
+        #sync.sync_genesis_contracts()
         #client = ContractingClient()
         #vk_book_contract = client.get_contract('vkbook')
 
