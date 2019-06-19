3255948bd617dacd5cd072a44b4a1c1b202228e6
Stuart Farmer <stuart@lamden.io>
2019-06-19 11:11:26 -0700

    deleted unused files

--- cilantro_ee/protocol/structures/bidict.py
+++
@@ -1,9 +0,0 @@
-class Bidict(dict):
-    def __setitem__(self, key, value):
-        super(Bidict, self).__setitem__(value, key)
-        super(Bidict, self).__setitem__(key, value)
-
-    def __delitem__(self, key):
-        val = self[key]
-        super(Bidict, self).__delitem__(val)
-        super(Bidict, self).__delitem__(key)
