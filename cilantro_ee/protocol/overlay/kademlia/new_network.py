3ef837d42c09d450dbb0d375f48adb18c2c4a665
Stuart Farmer <stuart@lamden.io>
2019-07-06 13:52:55 -0700

    much simpler network

--- cilantro_ee/protocol/overlay/kademlia/new_network.py
+++ cilantro_ee/protocol/overlay/kademlia/new_network.py
@@ -1,39 +1,54 @@
 from cilantro_ee.constants import conf
-from cilantro_ee.constants.ports import DHT_PORT
+from cilantro_ee.constants.ports import DHT_PORT, DISCOVERY_PORT
+from cilantro_ee.constants.overlay_network import PEPPER
+from cilantro_ee.protocol.overlay.kademlia import discovery
 from cilantro_ee.protocol.comm import services
 from cilantro_ee.protocol.wallet import Wallet
 
-from cilantro_ee.protocol.overlay.kademlia.routing import RoutingTable
-from cilantro_ee.protocol.overlay.kademlia.node import Node
-from cilantro_ee.protocol.overlay.kademlia.utils import digest
-
-from cilantro_ee.protocol.overlay.kademlia.discovery import DiscoveryServer, discover_nodes
-from cilantro_ee.constants.ports import DISCOVERY_PORT
-from cilantro_ee.constants.overlay_network import PEPPER
-from cilantro_ee.storage.vkbook import PhoneBook
-
 import asyncio
-
+import json
 import zmq
-import hashlib
 from cilantro_ee.logger.base import get_logger
-
 
 log = get_logger('NetworkService')
 
 
-def digest_from_vk(b: bytes):
-    h = hashlib.sha3_256()
-    h.update(b)
-    return h.digest()
+def ip_string_from_bytes(b: bytes):
+    b = bytearray(b)
+    ip = [str(byte) for byte in b[0:4]]
+    ip = '.'.join(ip)
+    return ip
 
-# returns node information when asked
-# emits events when new nodes are found?
-# emits events when ready
-# 1 req rep
-# 1 pub sub
-# socket services seperate classes
-# logic controlled by controller?
+
+def bytes_from_ip_string(ip: str):
+    b = ip.split('.')
+    return bytes(b)
+
+
+class KTable:
+    def __init__(self, data: dict, initial_peers={}, response_size=10):
+        self.data = data
+        self.peers = initial_peers
+        self.response_size = response_size
+
+    def find(self, key):
+        if key in self.data:
+            return self.data
+        elif key in self.peers:
+            return {
+                key: self.peers[key]
+            }
+        else:
+            # Do an XOR sort on all the keys to find neighbors
+            closest_peer_keys = sorted(self.peers.items(), key=lambda k: key ^ k)
+
+            # Only keep the response size number
+            closest_peer_keys = closest_peer_keys[:self.response_size]
+
+            # Dict comprehension
+            neighbors = {k: self.peers[k] for k in closest_peer_keys}
+
+            return neighbors
 
 
 class RPCServer(services.RequestReplyService):
@@ -41,108 +56,37 @@ class RPCServer(services.RequestReplyService):
         super().__init__(address=address, wallet=wallet, ctx=ctx)
 
         self.is_connected = False
+        self.wallet = wallet
 
-        # Set up routing table state for main service
-        digest = digest_from_vk(self.wallet.verifying_key())
-        self.dht_id = Node(node_id=digest,
-                           ip=self.ip,
-                           port=DHT_PORT,
-                           vk=self.wallet.verifying_key().hex())
-
-        self.routing_table = RoutingTable(self.dht_id)
-
-        # Set up event publishing service for aux services such as nodes joining and leaving?
-        self.event_publisher = self.ctx.socket(zmq.PUB)
-        self.event_publisher.bind(event_address)
+        data = {
+            self.wallet.verifying_key(): bytes_from_ip_string(conf.HOST_IP)
+        }
+        self.table = KTable(data=data)
 
     def handle_msg(self, msg):
-        # Cast to bytearray for easier manipulation
-        msg = bytearray(msg)
+        # Ping messages are empty and will eventually replace discovery
+        if len(msg) == 0:
+            return b''
+        elif len(msg) == 32:
+            # Try to find the key value
+            # Result will be a dictionary.
+            return self.table.find(msg)
 
-        # First byte determines command
-        command = msg.pop(0)
+        elif len(msg) == 32 + 4:
+            asyncio.ensure_future(self.handle_join(msg))
 
-        if command == 0 and len(msg) == 68:
-            ip = [str(byte) for byte in msg[0:4]]
-            ip = '.'.join(ip)
+    async def handle_join(self, msg):
+        vk = msg[:32]
+        ip = msg[32:]
 
-            sender_vk = msg[4:36]
-            requested_vk = msg[36:]
+        result = self.table.find(vk)
 
-            return self.rpc_find_ip(ip, sender_vk, requested_vk)
+        if vk not in result or result[vk] != ip:
+            # Ping discovery server
+            ip_string = ip_string_from_bytes(ip)
+            address = 'tcp://{}:{}'.format(ip_string, DISCOVERY_PORT)
+            _, responded_vk = await discovery.ping(address, pepper=PEPPER, timeout=1000)
 
-        elif command == 1:
-            # do the other one
-            return self.rpc_ping_ip()
-
-    async def rpc_find_ip(self, ip, sender_vk, requested_vk):
-        if self.is_connected and sender_vk == requested_vk:
-            await self.event_publisher.send_json({'event': 'node_online',
-                                                  'vk': sender_vk,
-                                                  'ip': ip})
-
-        requested_digest = digest_from_vk(requested_vk)
-        requested_node = Node(node_id=requested_digest,
-                              vk=requested_vk)
-
-        nodes = self.routing_table.find_node(requested_node)
-        return nodes
-
-    @staticmethod
-    def rpc_ping_ip():
-        return b''
-
-
-class NewNetwork:
-    def __init__(self, wallet, ctx:zmq.Context):
-        self.wallet = wallet
-        self.ctx = ctx
-
-        self.ip = conf.HOST_IP
-
-        # Configure node ID for DHT storage
-        digest = digest_from_vk(self.wallet.verifying_key())
-        self.dht_id = Node(node_id=digest,
-                           ip=self.ip,
-                           port=DHT_PORT,
-                           vk=self.wallet.verifying_key().hex())
-
-        self.routing_table = RoutingTable(self.dht_id)
-
-        discovery_address = 'tcp://*:{}'.format(DISCOVERY_PORT)
-        self.log.info('Setting up Discovery Server on {}.'.format(discovery_address))
-        self.discovery_server = DiscoveryServer(address=discovery_address,
-                                                wallet=self.wallet,
-                                                pepper=PEPPER.encode(),
-                                                ctx=self.ctx)
-
-        self.tasks = []
-
-        if self.wallet.verifying_key().hex() in PhoneBook.masternodes:
-            self.tasks.append(self.discovery_server.serve())
-
-    async def bootup(self):
-        ip_list = conf.BOOTNODES
-
-        # Remove our own IP so that we don't respond to ourselves.
-        if conf.HOST_IP in ip_list:
-            ip_list.remove(conf.HOST_IP)
-
-        log.info('Pinging {} for discovery...'.format(ip_list))
-
-        addrs = await discover_nodes(ip_list, pepper=PEPPER.encode(), ctx=self.ctx)
-
-        if len(addrs):
-            log.success('Found {} node(s). Putting them in the DHT.'.format(len(addrs)))
-
-            nodes = [Node(digest(vk), ip=ip, port=self.port, vk=vk) for ip, vk in addrs.items()]
-
-            if not self.discovery_server.running:
-                log.info('Discovery server was not running. Starting it now so others can find us.')
-                asyncio.ensure_future(self.discovery_server.serve())
-
-            log.success('Going into bootstrap!')
-            await self.bootstrap(nodes)
-
-        else:
-            raise Exception('Failed to discover any nodes.')
+            if responded_vk == vk:
+                # Valid response
+                self.table.peers[vk] = ip
