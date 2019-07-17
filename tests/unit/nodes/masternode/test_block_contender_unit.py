594e4cab5abdc5fa48a9a5644570b9bcdcd4ae80
Stuart Farmer <stuart@lamden.io>
2019-07-17 12:00:34 -0700

    get current quorum test coverage 100

--- tests/unit/nodes/masternode/test_block_contender_unit.py
+++ tests/unit/nodes/masternode/test_block_contender_unit.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 from cilantro_ee.nodes.masternode.block_contender import SubBlockGroup
 from cilantro_ee.storage.vkbook import VKBook
+import secrets
 
 
-def random_wallets():
-    pass
+def random_wallets(n=10):
+    return [secrets.token_hex(32) for _ in range(n)]
 
 
 class TestSubBlockGroup(TestCase):
@@ -52,4 +53,35 @@ class TestSubBlockGroup(TestCase):
         s.rh[s.best_rh] = {'A' * 64, 'B' * 64, 'C' * 64, 'D' * 64}
 
         self.assertEqual(s.get_current_quorum_reached(), s.max_quorum)
+
+    def test_current_quorum_returns_zero_when_no_result_has_enough_votes(self):
+        contacts = VKBook(delegates=['A' * 64, 'B' * 64, 'C' * 64, 'D' * 64], masternodes=['A' * 64],
+                          num_boot_del=3)
+
+        s = SubBlockGroup(0, 'A' * 64, contacts=contacts)
+
+        s.best_rh = 'B' * 64
+        another_result = 'C' * 64
+
+        s.rh[s.best_rh] = {'A' * 64}
+        s.rh[another_result] = {'B' * 64}
+
+        self.assertEqual(s.get_current_quorum_reached(), 0)
+
+    def test_current_quorum_returns_leading_vote_number_if_reduced_quorum_can_be_set(self):
+        delegates = random_wallets(100)
+
+        contacts = VKBook(delegates=delegates,
+                          masternodes=['A' * 64],
+                          num_boot_del=10)
+
+        s = SubBlockGroup(0, 'A' * 64, contacts=contacts)
+
+        s.best_rh = 'B' * 64
+        another_result = 'C' * 64
+
+        s.rh[s.best_rh] = set(delegates[:59])
+        s.rh[another_result] = {'B' * 64}
+
+        self.assertEqual(s.get_current_quorum_reached(), 59)
 
