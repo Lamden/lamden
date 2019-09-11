0df10f4c1b22f115e22d924a6e93bc7e5eac1e15
tejas <tejas@lamden.io>
2019-09-11 15:02:32 -0700

    Revert "upgrade contract"
    This reverts commit f599bf902410dc9f45881cffc5e66813cd09b47c.

--- cilantro_ee/contracts/genesis/vkbook.s.py
+++ cilantro_ee/contracts/genesis/vkbook.s.py
@@ -41,13 +41,6 @@ def get_delegates():
 def get_masternodes():
     return masternode_list.get()
 
-@export
-def check_master(vk):
-    return vk in masternode_list.get()
-
-@export
-def check_delegate(vk):
-    return vk in delegate_list.get()
 
 @export
 def get_witnesses():
@@ -80,6 +73,5 @@ def get_stamps_enabled():
 @export
 def get_nonces_enabled():
     return nonces_enabled.get()
-
 
 
