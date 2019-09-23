aa288dafc74736afb8b39080e66af7a1d11abc7d
Stuart Farmer <stuart@lamden.io>
2019-09-23 16:08:22 -0700

    tests setup

--- cilantro_ee/core/rewards.py
+++ cilantro_ee/core/rewards.py
@@ -1,6 +1,6 @@
 from cilantro_ee.storage.state import MetaDataStorage
 from contracting.client import ContractingClient
-
+from cilantro_ee.storage.vkbook import PhoneBook
 import capnp
 import os
 from cilantro_ee.messages import capnp as schemas
@@ -15,18 +15,47 @@ class RewardManager:
         self.driver = driver
         self.client = client
 
+        self.stamp_contract = self.client.get_contract('stamp_cost')
+        self.reward_contract = self.client.get_contract('rewards')
+        self.currency_contract = self.client.get_contract('currency')
+
+        assert self.stamp_contract is not None, 'Stamp contract not in state.'
+        assert self.reward_contract is not None, 'Reward contract not in state.'
+        assert self.currency_contract is not None, 'Currency contract not in state.'
+
     def issue_rewards(self):
         master_ratio, delegate_ratio, burn_ratio, foundation_ratio = self.reward_ratio
+        pending_rewards = self.get_pending_rewards()
 
-    @property
-    def pending_rewards(self):
-        return self.driver.get(PENDING_REWARDS_KEY)
+        masters = PhoneBook.delegates
+        delegates = PhoneBook.delegates
+
+        master_reward = (master_ratio * pending_rewards) / len(masters)
+        delegate_reward = (delegate_ratio * pending_rewards) / len(delegates)
+        foundation_reward = foundation_ratio * pending_rewards
+
+        for m in masters:
+            self.add_to_balance(vk=m, amount=master_reward)
+
+        for d in delegates:
+            self.add_to_balance(vk=d, amount=delegate_reward)
+
+    def add_to_balance(self, vk, amount):
+        current_balance = self.currency_contract.quick_read(variable='balances', key=vk) or 0
+        self.currency_contract.quick_write(variable='balances', key=vk, value=amount + current_balance)
+
+    def get_pending_rewards(self):
+        return self.driver.get(PENDING_REWARDS_KEY) or 0
+
+    def set_pending_rewards(self, value):
+        self.driver.set(PENDING_REWARDS_KEY, value=value)
 
     @property
     def stamps_per_tau(self):
-        pass
+        return self.stamp_contract.quick_read('S', 'rate')
 
-    def stamps_in_block(self, block):
+    @staticmethod
+    def stamps_in_block(block):
         total = 0
 
         for sb in block.subBlocks:
@@ -36,20 +65,12 @@ class RewardManager:
         return total
 
     def add_pending_rewards(self, block):
-        current_rewards = self.pending_rewards
+        current_rewards = self.get_pending_rewards()
         used_stamps = self.stamps_in_block(block)
 
-
+        rewards_as_tau = used_stamps / self.stamps_per_tau
+        self.set_pending_rewards(current_rewards + rewards_as_tau)
 
     @property
     def reward_ratio(self):
-        return [0, 0, 0, 0]
-
-    def get_masternodes(self):
-        pass
-
-    def get_delegates(self):
-        pass
-
-    def get_foundation(self):
-        pass
+        return self.reward_ratio.quick_read('value')
--- tests/unit/test_rewards.py
+++ tests/unit/test_rewards.py
@@ -1,11 +1,38 @@
 from unittest import TestCase
 from cilantro_ee.core.rewards import RewardManager
 from tests import random_txs
-
-
+from cilantro_ee.storage.state import MetaDataStorage
+from contracting.client import ContractingClient
+from cilantro_ee.contracts import genesis
+import os
 class TestRewards(TestCase):
-    def test_init(self):
-        r = RewardManager()
+    def setUp(self):
+
+        self.driver = MetaDataStorage()
+        self.client = ContractingClient()
+
+        genesis_path = os.path.dirname(genesis.__file__)
+
+        with open(os.path.join(genesis_path, 'election_house.s.py')) as f:
+            c = f.read()
+            self.client.submit(c, name='election_house')
+
+        with open(os.path.join(genesis_path, 'currency.s.py')) as f:
+            c = f.read()
+            self.client.submit(c, name='currency')
+
+        with open(os.path.join(genesis_path, 'stamp_cost.s.py')) as f:
+            c = f.read()
+            self.client.submit(c, name='stamp_cost', owner='election_house', constructor_args={'initial_rate': 1_000_000})
+
+        with open(os.path.join(genesis_path, 'rewards.s.py')) as f:
+            c = f.read()
+            self.client.submit(c, name='rewards', owner='election_house')
+
+        self.r = RewardManager()
+
+    def tearDown(self):
+        self.driver.flush()
 
     def test_add_rewards(self):
         block = random_txs.random_block()
@@ -16,3 +43,7 @@ class TestRewards(TestCase):
             for tx in sb.transactions:
                 total += tx.stampsUsed
 
+        self.assertEqual(self.r.stamps_in_block(block), total)
+
+    def test_add_to_balance(self):
+        pass
