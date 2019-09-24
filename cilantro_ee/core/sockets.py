0041071b6c5cb2c6cc09183bce047da4b13e2dd0
Stuart Farmer <stuart@lamden.io>
2019-09-24 12:58:08 -0700

    swapped for socket book

--- cilantro_ee/core/sockets.py
+++ cilantro_ee/core/sockets.py
@@ -3,24 +3,37 @@ from cilantro_ee.storage.vkbook import PhoneBook
 import asyncio
 
 
-class MasternodeSockets:
-    def __init__(self, network=None):
-        # Mapping between VK and ZMQ socket
+class SocketBook:
+    def __init__(self, network: Network=None, phonebook_function: callable=None):
         self.network = network
+        self.phonebook_function = phonebook_function
         self.sockets = {}
 
     async def refresh(self):
-        pb_nodes = set(PhoneBook.masternodes)
+        pb_nodes = set(self.phonebook_function())
         current_nodes = set(self.sockets.keys())
 
-        to_add = self.new_nodes(pb_nodes, current_nodes)
+        # Delete / remove old nodes
         to_del = self.old_nodes(pb_nodes, current_nodes)
-
-        for node in to_add:
-            pass
 
         for node in to_del:
             self.remove_node(node)
+
+        # Add new nodes
+        to_add = self.new_nodes(pb_nodes, current_nodes)
+        coroutines = [self.network.find_node(client_address=self.network.peer_service_address,
+                                             vk_to_find=m) for m in to_add]
+
+        tasks = asyncio.gather(*coroutines)
+        loop = asyncio.get_event_loop()
+
+        if loop.is_running():
+            results = await asyncio.ensure_future(tasks)
+        else:
+            results = loop.run_until_complete(tasks)
+
+        for r in results:
+            self.sockets.update(r)
 
     @staticmethod
     def new_nodes(phone_book_nodes, current_nodes):
@@ -36,3 +49,58 @@ class MasternodeSockets:
         if entry is not None:
             entry.close()
             del self.sockets[vk]
+
+    def get_socket_for_vk(self, vk):
+        return self.sockets.get(vk)
+
+
+class MasternodeSockets:
+    def __init__(self, network=None):
+        # Mapping between VK and ZMQ socket
+        self.network = network
+        self.sockets = {}
+
+    async def refresh(self):
+        pb_nodes = set(PhoneBook.masternodes)
+        current_nodes = set(self.sockets.keys())
+
+        # Delete / remove old nodes
+        to_del = self.old_nodes(pb_nodes, current_nodes)
+
+        for node in to_del:
+            self.remove_node(node)
+
+        # Add new nodes
+        to_add = self.new_nodes(pb_nodes, current_nodes)
+        coroutines = [self.network.find_node(client_address=self.network.peer_service_address,
+                                             vk_to_find=m) for m in to_add]
+
+        tasks = asyncio.gather(*coroutines)
+        loop = asyncio.get_event_loop()
+
+        if loop.is_running():
+            results = await asyncio.ensure_future(tasks)
+        else:
+            results = loop.run_until_complete(tasks)
+
+        for r in results:
+            self.sockets.update(r)
+
+
+    @staticmethod
+    def new_nodes(phone_book_nodes, current_nodes):
+        return phone_book_nodes - current_nodes
+
+    @staticmethod
+    def old_nodes(phone_book_nodes, current_nodes):
+        return current_nodes - phone_book_nodes
+
+    def remove_node(self, vk):
+        entry = self.sockets.get(vk)
+
+        if entry is not None:
+            entry.close()
+            del self.sockets[vk]
+
+    def get_socket_for_vk(self, vk):
+        return self.sockets.get(vk)
--- tests/unit/test_masternodeSockets.py
+++ tests/unit/test_masternodeSockets.py
@@ -1,29 +1,29 @@
 from unittest import TestCase
-from cilantro_ee.core.sockets import MasternodeSockets
+from cilantro_ee.core.sockets import MasternodeSockets, SocketBook
 import zmq
 
 
-class TestMasternodeSockets(TestCase):
+class TestSocketBook(TestCase):
     def test_new_nodes(self):
         current_nodes = {1, 2, 3, 4}
         all_nodes = {1, 2, 3, 4, 5, 6, 7, 8}
 
-        self.assertEqual(MasternodeSockets.new_nodes(all_nodes, current_nodes), {5, 6, 7, 8})
+        self.assertEqual(SocketBook.new_nodes(all_nodes, current_nodes), {5, 6, 7, 8})
 
     def test_old_nodes(self):
         current_nodes = {1, 2, 3, 4}
         all_nodes = {1, 2, 3, 4, 5, 6, 7, 8}
 
-        self.assertEqual(MasternodeSockets.old_nodes(all_nodes, current_nodes), set())
+        self.assertEqual(SocketBook.old_nodes(all_nodes, current_nodes), set())
 
     def test_old_nodes_actual_difference(self):
         current_nodes = {1, 2, 3, 4}
         all_nodes = {1, 4, 5, 6, 7, 8}
 
-        self.assertEqual(MasternodeSockets.old_nodes(all_nodes, current_nodes), {2, 3})
+        self.assertEqual(SocketBook.old_nodes(all_nodes, current_nodes), {2, 3})
 
     def test_remove_node(self):
-        m = MasternodeSockets()
+        m = SocketBook()
         ctx = zmq.Context()
         m.sockets = {'a': ctx.socket(zmq.PUB), 'b': ctx.socket(zmq.PUB)}
 
