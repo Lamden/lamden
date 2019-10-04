3f99e3ce17cc5c730c9312639a585bcfe2ee57f5
tejas <tejas@lamden.io>
2019-10-04 15:12:54 -0700

    wip upgrade test#2

--- tests/unit/test_ws.py
+++ tests/unit/test_ws.py
@@ -23,8 +23,33 @@ class TestWS(TestCase):
                          {'epoch_hash': '0000000000000000000000000000000000000000000000000000000000000000',
                           'blocks_until_next_epoch': 1})
 
+    def test_get_submission_methods(self):
+        _, response = app.test_client.get('/contracts/submission/methods')
+
+        method = 'submit_contract'
+        kwargs = ['name', 'code', 'owner', 'constructor_args']
+
+        methods = response.json.get('methods')
+
+        self.assertEqual(len(methods), 1)
+
+        test_method = methods[0]
+
+        self.assertEqual(method, test_method.get('name'))
+        self.assertListEqual(kwargs, test_method.get('arguments'))
+        self.assertEqual(response.status, 200)
+
+    def test_get_non_existent_contract_methods(self):
+        _, response = app.test_client.get('/contracts/huuuuuuuuupluh/methods')
+
+        self.assertEqual(response.status, 404)
+
+        error_message = response.json.get('error')
+
+        self.assertEqual(error_message, 'huuuuuuuuupluh does not exist')
+
     def test_contracts_api(self):
-        _, response = app.test_client.get('/contracts')
+        _, response = app.test_client.get('/contracts/vkbook')
         pass
 
     def test_blocks_api(self):
