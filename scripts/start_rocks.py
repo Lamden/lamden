d243ea0e5b0154e76ac20a01230f22ce13ac0f1c
tejas <tejas@lamden.io>
2019-12-05 11:07:20 -0800

    running rocks in background

--- scripts/start_rocks.py
+++ scripts/start_rocks.py
@@ -1,8 +1,10 @@
-import os, sys
+import os
+
 
 def start_rocks():
     print("Starting Rocks server...")
     os.system('export LC_ALL=C.UTF-8; export LANG=C.UTF-8; rocks serve &')
+
 
 if __name__ == '__main__':
     start_rocks()
