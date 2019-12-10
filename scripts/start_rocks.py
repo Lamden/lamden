a1f1986a04cabccf242ccd4fbbb2bec5f7a94c28
tejas <tejas@lamden.io>
2019-12-10 11:16:39 -0800

    rocks fix

--- scripts/start_rocks.py
+++ scripts/start_rocks.py
@@ -12,7 +12,7 @@ def start_rocks():
     print("Rocks using data directory: {}".format(ROCKS_DIR))
 
     # cmd = f"export LC_ALL=en_US.UTF-8; export LANG=C.UTF-8; rocks serve -d {ROCKS_DIR}"
-    cmd = f"export LC_ALL=C.UTF-8; export LANG=C.UTF-8; rocks serve -d {ROCKS_DIR}"
+    cmd = f"export LC_ALL=C.UTF-8; export LANG=C.UTF-8; rocks serve -d {ROCKS_DIR} &"
     # os.system('export LC_ALL=C.UTF-8; export LANG=C.UTF-8; rocks serve -d /Users/lamden/lamden/rocks')
     # os.system('export LC_ALL=en_US.UTF-8; export LANG=C.UTF-8; rocks serve -d ROCKS_DIR')
     os.system(cmd)
