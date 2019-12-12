34c2c1c8494d7f1f7e45969a45a6e6cd1bf8e121
tejas <tejas@lamden.io>
2019-12-12 15:00:55 -0800

    dev-requirement cleanup

--- setup.py
+++ setup.py
@@ -18,26 +18,39 @@ setup(
     name='cilantro_ee',
     version=__version__,
     packages=find_packages(exclude=['docs', 'ops', 'docker', 'deprecated']),
+
+    # Note install requirements have to be same as dev-requirement.txt (for aws)
     install_requires=[
+        # utils
+        'coloredlogs',
+        'checksumdir==1.1.7',
+        'pathspec',
+        'vprof',
+
         'Cython==0.29',
+
+        # protocol
         'pycapnp==0.6.3',
-        #PyNaCl==1.2.1
         'pyzmq==17.0.0',
         'requests==2.20.0',
         'uvloop==0.9.1',
-        'u-msgpack-python==2.5.0',
-        'yarl==1.1.0',
-        'rocks',
-        'contracting',
-        'click',
-        'simple-crypt',
+
+        # webserver
+        'aiohttp',
         'sanic==19.6.3',
+        'secure',
+        'sanic-limiter==0.1.3',
+        'Sanic-Cors==0.9.9.post1',
+
+        # DB
+        'rocks',
+        'contracting',s
         'pymongo==3.9',
-        'checksumdir==1.1.7',
+        'redis==3.2.0'
+        
+        # CLI
+        'Click==7.0',
     ],
-    extras_require={
-        'dev': open('dev-requirements.txt').readlines()
-    },
     entry_points={
         'console_scripts': [
             'cilantro_ee=__main__:main'
