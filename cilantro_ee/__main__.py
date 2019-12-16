3e0aac12e605c5c5e59270de7dcc2e1d5a72b1b7
tejas <tejas@lamden.io>
2019-12-16 11:39:59 -0800

    trigger working

---
+++ cilantro_ee/__main__.py
@@ -0,0 +1,13 @@
+import runpy
+import os
+
+def main():
+    print("##########")
+    pass
+    #runpy.run_path(path_name = '/cilantro_ee/scripts/setup_node.py')
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    print("This is  main")
+    main()
