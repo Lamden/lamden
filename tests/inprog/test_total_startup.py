cf319f5bdebdacba93c6d20489f99e6a5ba96aa0
Stuart Farmer <stuart@lamden.io>
2020-01-13 15:51:57 -0800

    8X8 NETWORK BOOTS!!!

--- tests/inprog/test_total_startup.py
+++ tests/inprog/test_total_startup.py
@@ -9,11 +9,13 @@ from cilantro_ee.nodes.delegate.delegate import Delegate
 
 import os
 
+
 def make_ipc(p):
     try:
         os.mkdir(p)
     except:
         pass
+
 
 class TestTotalEndToEnd(TestCase):
     def setUp(self):
@@ -31,6 +33,185 @@ class TestTotalEndToEnd(TestCase):
         # 2 bootnodes
         # 2 mns, 2 delegates
 
+        bootnodes = ['ipc:///tmp/n1', 'ipc:///tmp/n3']
+
+        mnw1 = Wallet()
+        mnw2 = Wallet()
+        mnw3 = Wallet()
+        mnw4 = Wallet()
+        mnw5 = Wallet()
+        mnw6 = Wallet()
+        mnw7 = Wallet()
+        mnw8 = Wallet()
+
+        dw9 = Wallet()
+        dw10 = Wallet()
+        dw11 = Wallet()
+        dw12 = Wallet()
+        dw13 = Wallet()
+        dw14 = Wallet()
+        dw15 = Wallet()
+        dw16 = Wallet()
+
+        constitution = {
+            "masternodes": {
+                "vk_list": [
+                    mnw1.verifying_key().hex(),
+                    mnw2.verifying_key().hex(),
+                    mnw3.verifying_key().hex(),
+                    mnw4.verifying_key().hex(),
+                    mnw5.verifying_key().hex(),
+                    mnw6.verifying_key().hex(),
+                    mnw7.verifying_key().hex(),
+                    mnw8.verifying_key().hex(),
+                ],
+                "min_quorum": 6
+            },
+            "delegates": {
+                "vk_list": [
+                    dw9.verifying_key().hex(),
+                    dw10.verifying_key().hex(),
+                    dw11.verifying_key().hex(),
+                    dw12.verifying_key().hex(),
+                    dw13.verifying_key().hex(),
+                    dw14.verifying_key().hex(),
+                    dw15.verifying_key().hex(),
+                    dw16.verifying_key().hex(),
+                ],
+                "min_quorum": 8
+            },
+            "witnesses": {},
+            "schedulers": {},
+            "notifiers": {},
+            "enable_stamps": False,
+            "enable_nonces": False
+        }
+
+        n1 = '/tmp/n1'
+        make_ipc(n1)
+        mn1 = Masternode(wallet=mnw1, ctx=self.ctx, socket_base=f'ipc://{n1}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8081)
+
+        n2 = '/tmp/n2'
+        make_ipc(n2)
+        mn2 = Masternode(wallet=mnw2, ctx=self.ctx, socket_base=f'ipc://{n2}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8082)
+
+        n3 = '/tmp/n3'
+        make_ipc(n3)
+        mn3 = Masternode(wallet=mnw3, ctx=self.ctx, socket_base=f'ipc://{n3}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8083)
+
+        n4 = '/tmp/n4'
+        make_ipc(n4)
+        mn4 = Masternode(wallet=mnw4, ctx=self.ctx, socket_base=f'ipc://{n4}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8084)
+
+        n5 = '/tmp/n5'
+        make_ipc(n5)
+        mn5 = Masternode(wallet=mnw5, ctx=self.ctx, socket_base=f'ipc://{n5}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8085)
+
+        n6 = '/tmp/n6'
+        make_ipc(n6)
+        mn6 = Masternode(wallet=mnw6, ctx=self.ctx, socket_base=f'ipc://{n6}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8086)
+
+        n7 = '/tmp/n7'
+        make_ipc(n7)
+        mn7 = Masternode(wallet=mnw7, ctx=self.ctx, socket_base=f'ipc://{n7}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8087)
+
+        n8 = '/tmp/n8'
+        make_ipc(n8)
+        mn8 = Masternode(wallet=mnw8, ctx=self.ctx, socket_base=f'ipc://{n8}', bootnodes=bootnodes,
+                         constitution=constitution, webserver_port=8088)
+
+        n9 = '/tmp/n9'
+        make_ipc(n9)
+        d9 = Delegate(wallet=dw9, ctx=self.ctx, socket_base=f'ipc://{n9}', constitution=constitution,
+                      bootnodes=bootnodes)
+
+        n10 = '/tmp/n10'
+        make_ipc(n10)
+        d10 = Delegate(wallet=dw10, ctx=self.ctx, socket_base=f'ipc://{n10}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n11 = '/tmp/n11'
+        make_ipc(n11)
+        d11 = Delegate(wallet=dw11, ctx=self.ctx, socket_base=f'ipc://{n11}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n12 = '/tmp/n12'
+        make_ipc(n12)
+        d12 = Delegate(wallet=dw12, ctx=self.ctx, socket_base=f'ipc://{n12}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n13 = '/tmp/n13'
+        make_ipc(n13)
+        d13 = Delegate(wallet=dw13, ctx=self.ctx, socket_base=f'ipc://{n13}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n14 = '/tmp/n14'
+        make_ipc(n14)
+        d14 = Delegate(wallet=dw14, ctx=self.ctx, socket_base=f'ipc://{n14}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n15 = '/tmp/n15'
+        make_ipc(n15)
+        d15 = Delegate(wallet=dw15, ctx=self.ctx, socket_base=f'ipc://{n15}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+        n16 = '/tmp/n16'
+        make_ipc(n16)
+        d16 = Delegate(wallet=dw16, ctx=self.ctx, socket_base=f'ipc://{n16}', constitution=constitution,
+                       bootnodes=bootnodes)
+
+
+        # should test to see all ready signals are recieved
+        tasks = asyncio.gather(
+            mn1.start(),
+            mn2.start(),
+            mn3.start(),
+            mn4.start(),
+            mn5.start(),
+            mn6.start(),
+            mn7.start(),
+            mn8.start(),
+            d9.start(),
+            d10.start(),
+            d11.start(),
+            d12.start(),
+            d13.start(),
+            d14.start(),
+            d15.start(),
+            d16.start(),
+        )
+
+        async def run():
+            await tasks
+            await asyncio.sleep(5)
+            mn1.stop()
+            mn2.stop()
+            mn3.stop()
+            mn4.stop()
+            mn5.stop()
+            mn6.stop()
+            mn7.stop()
+            mn8.stop()
+            d9.stop()
+            d10.stop()
+            d11.stop()
+            d12.stop()
+            d13.stop()
+            d14.stop()
+            d15.stop()
+            d16.stop()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(run())
+
+    def test_large_network_bootup(self):
         bootnodes = ['ipc:///tmp/n1', 'ipc:///tmp/n3']
 
         mnw1 = Wallet()
@@ -83,7 +264,6 @@ class TestTotalEndToEnd(TestCase):
         d2 = Delegate(wallet=dw2, ctx=self.ctx, socket_base=f'ipc://{n4}',
                       constitution=constitution, bootnodes=bootnodes)
 
-
         # should test to see all ready signals are recieved
         tasks = asyncio.gather(
             mn1.start(),
@@ -95,7 +275,6 @@ class TestTotalEndToEnd(TestCase):
         async def run():
             await tasks
             await asyncio.sleep(5)
-            print('STOP')
             mn1.stop()
             mn2.stop()
             d1.stop()
