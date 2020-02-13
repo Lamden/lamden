8e5c3afad7276bdef87a1c8e3c8b5ee868b30a61
tejas <tejas@lamden.io>
2020-02-13 03:04:41 -0800

    nw rdy logic

--- cilantro_ee/contracts/genesis/upgrade.s.py
+++ cilantro_ee/contracts/genesis/upgrade.s.py
@@ -17,11 +17,16 @@ upg_window = Variable()
 mn_vote = Variable()
 dl_vote = Variable()
 
+
+mn_rdy = Variable()
+dl_rdy = Variable()
+
 tot_mn = Variable()
 tot_dl = Variable()
 
 # Results
 upg_consensus = Variable()
+nw_ready = Variable()
 
 
 @construct
@@ -64,16 +69,24 @@ def vote(vk):
         if check_vote_state():
             reset_contract()
 
+@export
+def ready(vk):
+    if upg_lock.get():
+        if vk in election_house.current_value_for_policy('masternodes'):
+            mn_rdy.set(mn_rdy.get() + 1)
+        if vk in election_house.current_value_for_policy('delegates'):
+            dl_rdy.set(dl_rdy.get() + 1)
 
 def check_vote_state():
     mn = tot_mn.get()
     dl = tot_dl.get()
 
-    if (mn_vote > (mn*2)/3) and (dl_vote > (dl*2)/3):
+    if (mn_vote.get() > (mn*2)/3) and (dl_vote.get() > (dl*2)/3):
         upg_consensus.set(True)
-        return True
-    else:
-        return False
+
+    if (mn_rdy.get() > (mn * 2) / 3) and (dl_rdy.get() > (dl * 2) / 3):
+        nw_ready.set(True)
+
 
 
 def reset_contract():
