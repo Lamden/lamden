bd54666d58a37024085fbb91c3ceb0cc478f20a7
Stuart Farmer <stuart@lamden.io>
2020-02-17 12:39:59 -0800

    start rocks process

--- cilantro_ee/cli/start_dbs.py
+++ cilantro_ee/cli/start_dbs.py
@@ -1,8 +1,13 @@
 import subprocess
+from rocks.client import RocksDBClient, RocksServerOfflineError
 
 
 def start_rocks():
-    subprocess.Popen(['rocks', 'serve'], stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    c = RocksDBClient()
+    try:
+        c.ping()
+    except RocksServerOfflineError:
+        subprocess.Popen(['rocks', 'serve'], stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
 
 
 def start_mongo():
