31d77e717bc0617eb5be161ec779e081e60793fb
Stuart Farmer <stuart@lamden.io>
2020-02-17 14:32:20 -0800

    start databases

--- cilantro_ee/cli/start_dbs.py
+++ cilantro_ee/cli/start_dbs.py
@@ -1,14 +1,24 @@
 import subprocess
 from rocks.client import RocksDBClient, RocksServerOfflineError
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 
 def start_rocks():
-    c = RocksDBClient()
     try:
+        c = RocksDBClient()
         c.ping()
     except RocksServerOfflineError:
-        subprocess.Popen(['rocks', 'serve'], stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+        subprocess.Popen(['rocks', 'serve'],
+                         stdout=open('/dev/null', 'w'),
+                         stderr=open('/dev/null', 'w'))
 
 
 def start_mongo():
-    subprocess.Popen(['mongod', '--dbpath ~/blocks', '--logpath /dev/null', '--bind_ip_all'], stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    try:
+        c = MongoClient(serverSelectionTimeoutMS=200)
+        c.server_info()
+    except ServerSelectionTimeoutError:
+        subprocess.Popen(['mongod', '--dbpath ~/blocks', '--logpath /dev/null', '--bind_ip_all'],
+                         stdout=open('/dev/null', 'w'),
+                         stderr=open('/dev/null', 'w'))
