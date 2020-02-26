203225732bd996025f9a8f3928d93ebf192b5222
Stuart Farmer <stuart@lamden.io>
2020-02-26 10:56:56 -0800

    Merge branches 'rel_gov_debug' and 'rel_jan_2020' of https://github.com/Lamden/cilantro-enterprise into rel_gov_debug
    # Conflicts:
    #	cilantro_ee/nodes/commands/cmd.py

--- tests/unit/test_upgrade_contract.py
+++ tests/unit/test_upgrade_contract.py
@@ -12,12 +12,13 @@ class TestUpgradeContract(unittest.TestCase):
         self.client = ContractingClient()
 
         # Sync contracts
-        sync.submit_from_genesis_json_file(cilantro_ee.contracts.__path__[0] + '/genesis.json')
+        sync.submit_from_genesis_json_file(cilantro_ee.contracts.__path__[0] + '/genesis.json', client=self.client)
         sync.submit_node_election_contracts(
             initial_masternodes=['stu', 'raghu', 'steve'],
             boot_mns=2,
             initial_delegates=['tejas', 'alex'],
             boot_dels=3,
+            client=self.client
         )
 
     def tearDown(self):
