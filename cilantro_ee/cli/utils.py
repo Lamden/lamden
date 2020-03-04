36b78444cff72536f674715c13d897d9f8a801cf
tejas <tejas@lamden.io>
2020-03-04 15:31:19 -0800

    reorg pkg utilities

--- cilantro_ee/cli/utils.py
+++ cilantro_ee/cli/utils.py
@@ -1,4 +1,7 @@
+import os
 import ipaddress
+from checksumdir import dirhash
+
 
 def validate_ip(address):
     try:
@@ -7,3 +10,18 @@ def validate_ip(address):
         return ip
     except ValueError:
         print('address/netmask is invalid: %s' % address)
+
+
+def build_pepper(pkg_dir_path=os.environ.get('CIL_PATH')):
+    pepper = dirhash(pkg_dir_path, 'sha256', excluded_extensions = ['pyc'])
+    print(pepper)
+    return pepper
+
+
+def verify_cil_pkg(pkg_hash):
+    current_pepper = build_pepper(pkg_dir_path = os.environ.get('CIL_PATH'))
+
+    if current_pepper == pkg_hash:
+        return True
+    else:
+        return False
