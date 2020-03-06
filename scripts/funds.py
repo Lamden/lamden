6a28320b6b10d097e08a3bcfda2c69da46156838
tejas <tejas@lamden.io>
2020-03-06 14:04:51 -0800

    funds #2

--- scripts/funds.py
+++ scripts/funds.py
@@ -26,15 +26,31 @@ def send_tx(sender, contract, function, kwargs={}):
     tx.sign(sender.signing_key())
     packed_tx = tx.serialize()
     res = submit_transaction(packed_tx)
-    print(res.text)
+    #print(res.text)
 
 
-def get_funds():
+def get_funds_mn():
     send_tx(foundation, 'currency', 'transfer',
             {
                 'amount': 1_000_000,
                 'to': '0273b7051a137d1a5ee3d615cad20e8573ec2615da69c845085d6c6e45200482'
             })
+    return True
 
 
-get_funds()
+def get_funds_dl():
+    send_tx(foundation, 'currency', 'transfer',
+            {
+                'amount': 1_000_000,
+                'to': 'b6c26304d802140992bcd040538c5e9c2bc85644b620260dca838d5e91f0fdaa'
+            })
+    return True
+
+
+def main():
+    res_m = get_funds_mn()
+    res_d = get_funds_dl()
+
+
+if __name__ == "__main__":
+    main()
