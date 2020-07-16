7bee41f9d6143a687d2004acb702fa212fb534ca
Stuart Farmer <stuart@lamden.io>
2020-07-16 13:39:37 -0700

    updated install scripe

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,13 +4,18 @@ sudo apt-get install python3-pip -y
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt-get update
 sudo apt-get install -y mongodb-org
+
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
 echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
 sudo systemctl start mongod
+sudo systemctl enable mongod
 
 sudo apt-get install haveged -y
 systemctl start haveged
