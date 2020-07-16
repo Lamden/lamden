7c8727c8cd2c1f124569e11426d765ec26b2724c
Stuart Farmer <stuart@lamden.io>
2020-07-15 17:48:39 -0700

    update install

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install python3-pip -y
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get install -y mongodb-org
@@ -12,3 +15,5 @@ sudo systemctl start mongod
 sudo apt-get install haveged -y
 systemctl start haveged
 systemctl enable haveged
+
+pip3 install lamden
