b2ce8378d1190d5fe18bda68f392336d3eedfcad
Stuart Farmer <stuart@lamden.io>
2020-09-18 21:21:11 -0700

    submission update

--- lamden/contracts/submission.s.py
+++ lamden/contracts/submission.s.py
@@ -1,6 +1,6 @@
 @__export('submission')
 def submit_contract(name: str, code: str, owner: Any=None, constructor_args: dict={}):
-    assert not name.isdigit() and all(c.isalnum() or c == '_' for c in name), 'Invalid contract name!'
+    assert not name.isdigit() and all(c.isalnum() or c == '_' for c in name), 'Invalid contract name!!'
     __Contract().submit(name=name, code=code, owner=owner, constructor_args=constructor_args, developer=ctx.caller)
 
 
