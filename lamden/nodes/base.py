121a5a72029d3e85f98b554716942b6fb1a82155
Stuart Farmer <stuart@lamden.io>
2020-10-08 16:48:40 -0700

    clear

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -211,9 +211,8 @@ class Node:
             return False
 
         # Get current metastate
-
-        #if len(block['subblocks']) < 1:
-        #    return False
+        if len(block['subblocks']) < 1:
+           return False
 
         # Test if block contains the same metastate
         if block['number'] != self.current_height + 1:
