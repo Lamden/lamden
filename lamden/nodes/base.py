f5ff2f776535c296736e55940d4c0b9b60200d76
Stuart Farmer <stuart@lamden.io>
2020-10-08 15:55:10 -0700

    lt

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -216,7 +216,7 @@ class Node:
         #    return False
 
         # Test if block contains the same metastate
-        if block['number'] != self.current_height + 1:
+        if block['number'] < self.current_height + 1:
             self.log.info(f'Block #{block["number"]} != {self.current_height + 1}. '
                           f'Node has probably already processed this block. Continuing.')
             return False
