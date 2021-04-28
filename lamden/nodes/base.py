5ab7062f1bd1a7acb365698f72f4aab1a767b482
jeffwscott <jeffscott@outlook.com>
2021-04-28 16:17:50 -0400

    more updates

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -239,7 +239,7 @@ class Node:
             stamp_cost=self.client.get_var(contract='stamp_cost', variable='S', arguments=['value'])
         )
 
-        self.log.info(results['state'])
+        self.log.debug(results)
 
         block = block_from_subblocks(results, self.current_hash, self.current_height + 1)
         self.process_new_block(block)
