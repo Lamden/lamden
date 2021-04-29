e368c90e2e2adf842c7cc8c2da74833c0c84e452
jeffwscott <jeffscott@outlook.com>
2021-04-29 18:25:40 -0400

    more updates

--- lamden/nodes/work.py
+++ lamden/nodes/work.py
@@ -28,7 +28,7 @@ class WorkValidator(router.Processor):
 
 
     async def process_message(self, msg):
-        self.log.info(f'Received work from {msg["sender"][:8]}')
+        self.log.info(f'Received work from {msg["sender"][:8]} {msg["hlc_timestamp"]} {msg["tx"]["metadata"]["signature"][:12] }')
         ## self.log.info(msg)
 
         #if msg["sender"] == self.wallet.verifying_key:
