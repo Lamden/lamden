78b11eb934d60fe2bf2f5d5485fc49cfaf638cdb
jeffwscott <jeffscott@outlook.com>
2021-04-30 09:41:08 -0400

    more updates

--- reset.sh
+++ reset.sh
@@ -2,5 +2,6 @@
 killall lamden
 git pull
 rm -r ~/cilsocks/
-cd ../contracting/ && git pull && python3 setup.py develop && cd ../lamden
 python3 -m pip install lmdb
+cd ../contracting/ && git pull && python3 setup.py develop && cd ../lamden
+
