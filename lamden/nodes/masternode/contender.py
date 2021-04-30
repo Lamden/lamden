ee2f230700fa0b022d7cc20936f77af2d851b50b
jeffwscott <jeffscott@outlook.com>
2021-04-30 14:55:01 -0400

    more updates

--- lamden/nodes/masternode/contender.py
+++ lamden/nodes/masternode/contender.py
@@ -49,7 +49,9 @@ class SBCInbox(router.Processor):
                 if self.validation_results[tx['hlc_timestamp']][msg['signer']]:
                     self.log.error(f'Already recieved results from {msg["signer"]} for {tx["hlc_timestamp"]}')
                     return
-
+                self.log.debug(self.validation_results)
+                self.log.debug(msg[i])
+                self.log.debug(msg[i]['signer'])
                 self.validation_results[tx['hlc_timestamp']][msg[i]['signer']] = msg[i]
 
     def sbc_is_valid(self, sbc, sb_idx=0):
