fcefab04fd26223f5a06b90fa95f3ab8dd083511
jeffwscott <jeffscott@outlook.com>
2021-05-05 10:51:11 -0400

    more updates

--- lamden/nodes/filequeue.py
+++ lamden/nodes/filequeue.py
@@ -3,16 +3,19 @@ import uuid
 import shutil
 import os
 from contracting.db.encoder import decode
+from lamden.logger.base import get_logger
 
 class FileQueue:
     EXTENSION = '.tx'
 
     def __init__(self, root='./txs'):
+        self.log = get_logger('FILE QUEUE')
         self.root = Path(root)
         self.root.mkdir(parents=True, exist_ok=True)
 
     def append(self, tx):
         name = str(uuid.uuid4()) + self.EXTENSION
+        self.log(f'Saving to {self.root.joinpath(name)}')
         with open(self.root.joinpath(name), 'wb') as f:
             f.write(tx)
 
