3b43d50feaf6de2717fc3bc21e948440f4f431e4
jeffwscott <jeffscott@outlook.com>
2021-05-05 16:55:01 -0400

    more updates

--- lamden/nodes/validation_queue.py
+++ lamden/nodes/validation_queue.py
@@ -42,7 +42,7 @@ class ValidationQueue:
             self.validation_results[hlc_timestamp] = {}
             self.validation_results[hlc_timestamp]['delegate_solutions'] = {}
 
-        self.validation_results[hlc_timestamp]['delegate_solutions'][node_vk] = msg[0]
+        self.validation_results[hlc_timestamp]['delegate_solutions'][node_vk] = msg
 
     async def process_next(self):
         self.needs_validation_queue.sort()
