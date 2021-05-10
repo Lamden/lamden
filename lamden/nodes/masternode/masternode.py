e52077024539b6600f1cfaede31b2f52145aa754
jeffwscott <jeffscott@outlook.com>
2021-05-10 10:01:12 -0400

    more updates

--- lamden/nodes/masternode/masternode.py
+++ lamden/nodes/masternode/masternode.py
@@ -25,7 +25,7 @@ class BlockService(router.Processor):
 
     async def process_message(self, msg):
         response = None
-        mn_logger.debug('Got a msg')
+        # mn_logger.debug('Got a msg')
         if primatives.dict_has_keys(msg, keys={'name', 'arg'}):
             if msg['name'] == base.GET_BLOCK:
                 response = self.get_block(msg)
