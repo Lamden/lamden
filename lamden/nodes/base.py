2c12c7a294aeba71df029fd793331b989c709400
jeffwscott <jeffscott@outlook.com>
2021-05-21 09:46:17 -0400

    better debug statements

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -286,21 +286,24 @@ class Node:
             self.log.debug("Running check_tx_queue")
             if len(self.file_queue) > 0:
                 await self.send_tx_to_network(self.file_queue.pop(0))
-            asyncio.sleep(0)
+            else:
+                asyncio.sleep(0)
 
     async def check_main_processing_queue(self):
         while True:
-            self.log.debug("Running check_tx_queue")
+            self.log.debug("Running check_main_processing_queue")
             if len(self.main_processing_queue) > 0:
                 await self.process_main_queue()
-            asyncio.sleep(0)
+            else:
+                asyncio.sleep(0)
 
     async def check_validation_queue(self):
         while True:
-            self.log.debug("Running check_tx_queue")
+            self.log.debug("Running check_validation_queue")
             if len(self.validation_queue) > 0:
                 await self.validation_queue.process_next()
-            asyncio.sleep(0)
+            else:
+                asyncio.sleep(0)
 
     '''
     async def loop(self):
