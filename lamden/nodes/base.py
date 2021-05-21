dc7b6e3ad0667f57e1a1857f819cea9febbab21e
jeffwscott <jeffscott@outlook.com>
2021-05-21 12:59:56 -0400

    reduce processing delay

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -111,7 +111,7 @@ class Node:
                  genesis_path=contracts.__path__[0], reward_manager=rewards.RewardManager(), nonces=storage.NonceStorage(), parallelism=4):
 
         self.consensus_percent = 51
-        self.processing_delay = 2
+        self.processing_delay = 1
 
         self.driver = driver
         self.nonces = nonces
