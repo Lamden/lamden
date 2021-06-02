4149093aca39a8efb051865d8f451a82f3e2cd2e
jeffwscott <jeffscott@outlook.com>
2021-06-02 12:39:27 -0400

    block doesn't have tx info, debug value of results to get it

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -463,8 +463,7 @@ class Node:
             self.log.error("--------------------------------------------------------------------")
 
         block = block_from_subblocks(subblocks, self.current_hash, self.current_height + 1)
-        self.log.debug(results)
-        self.process_new_block(block, "000")
+        self.process_new_block(block, results['subblocks'][0]['transactions'][0]['hlc_timestamp'])
 
     def save_cached_state(self, results):
         bc = contender.BlockContender(total_contacts=1, total_subblocks=1)
