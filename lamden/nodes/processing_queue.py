4a269244358cd58fb7ed01299907fc0447b4e286
jeffwscott <jeffscott@outlook.com>
2021-06-02 11:36:08 -0400

    send solution as string and not list

--- lamden/nodes/processing_queue.py
+++ lamden/nodes/processing_queue.py
@@ -64,7 +64,7 @@ class ProcessingQueue:
                 'file': 'processing_queue',
                 'event': 'processed_from_main_queue',
                 'hlc_timestamp': tx['hlc_timestamp'],
-                'my_solution': results[0]['merkle_tree']['leaves'],
+                'my_solution': results[0]['merkle_tree']['leaves'][0],
                 'system_time': time.time()
             }))
 
