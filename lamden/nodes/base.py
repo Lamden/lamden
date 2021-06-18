53f880d66757cd3a52f6745aafed6332acb56c95
jeffwscott <jeffscott@outlook.com>
2021-06-18 11:41:12 -0400

    fix error in base.rollback method

--- lamden/nodes/base.py
+++ lamden/nodes/base.py
@@ -451,7 +451,7 @@ class Node:
         self.driver.rollback()
 
         # Add transactions I already processed back into the main_processing queue
-        for key, value in self.main_processing_queue.validation_results:
+        for key, value in self.validation_queue.validation_results:
             # Add the tx info back into the main processing queue
             self.main_processing_queue.append(tx=value['transaction_processed'])
 
