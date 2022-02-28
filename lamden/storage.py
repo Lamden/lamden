46f46795a0edcb4825a5de7f82b9aa328a9f4e90
Nikita Tokariev <mykyta.tokariev@gmail.com>
2022-02-27 23:04:27 -0700

    update key format according to new fs storage driver

--- lamden/storage.py
+++ lamden/storage.py
@@ -14,8 +14,8 @@ import os
 
 import shutil
 
-BLOCK_HASH_KEY = '_current_block_hash'
-BLOCK_NUM_HEIGHT = '_current_block_height'
+BLOCK_HASH_KEY = 'current_block.current_block_hash'
+BLOCK_NUM_HEIGHT = 'current_block.current_block_height'
 NONCE_KEY = '__n'
 PENDING_NONCE_KEY = '__pn'
 
