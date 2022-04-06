667ea4707881574ef0531229b296682dc19832a1
JeffWScott <jeffscott@outlook.com>
2022-04-06 15:00:23 -0400

    test router.py started

--- lamden/sockets/router.py
+++ lamden/sockets/router.py
@@ -1,11 +1,21 @@
+import asyncio
+import json
 from time import sleep
 import zmq
+import zmq.asyncio
 import threading
 
+from typing import Callable
+
 from lamden.logger.base import get_logger
-from zmq.auth.thread import ThreadAuthenticator
-from lamden.crypto import wallet
+from zmq.auth.asyncio import AsyncioAuthenticator
+from lamden.crypto.wallet import Wallet
 from lamden.crypto.z85 import z85_key
+
+EXCEPTION_NO_ADDRESS_SET = "Router address not set."
+EXCEPTION_NO_SOCKET = "No socket created."
+EXCEPTION_IP_NOT_TYPE_STR = "ip must be type string."
+EXCEPTION_PORT_NOT_TYPE_INT = "port must be type int"
 
 
 class CredentialsProvider(object):
@@ -38,18 +48,22 @@ class CredentialsProvider(object):
             return False
 
 
-class Router(threading.Thread):
-    def __init__(self, router_wallet: wallet, get_all_peers, callback = None, logger=None):
-        threading.Thread.__init__(self)
+class Router:
+    def __init__(self, router_wallet: Wallet = None, callback: Callable = None, logger=None):
         self.log = logger or get_logger('ROUTER')
-        self.ctx = zmq.Context()
+        self.ctx = None
         self.socket = None
         self.address = None
-        self.wallet = router_wallet
+        self.wallet = router_wallet or Wallet()
         self.running = False
         self.paused = False # For testing
-        self.cred_provider = CredentialsProvider(get_all_peers=get_all_peers, logger=self.log)
+        self.credentials_provider = CredentialsProvider()
+        self.auth = None
         self.callback = callback
+        self.loop = None
+
+        self.poller = None
+        self.poll_time = 500
 
     def __del__(self):
         print(f'[{self.log.name}][ROUTER] Destroyed')
@@ -59,70 +73,153 @@ class Router(threading.Thread):
     def is_paused(self):
         return self.paused
 
-    def run(self):
-        print(f'[{self.log.name}][ROUTER] Starting on: ' + self.address)
-        self.log.info('[ROUTER] Starting on: ' + self.address)
+    @property
+    def socket_is_bound(self) -> bool:
+        try:
+            return len(self.socket.LAST_ENDPOINT) > 0
+        except Exception as err:
+            return False
 
-        # Start an authenticator for this context.
+    @property
+    def socket_is_closed(self) -> bool:
+        try:
+            return self.socket.closed
+        except AttributeError:
+            return True
+
+    @property
+    def curve_server_setup(self) -> bool:
+        try:
+            return self.socket.CURVE_SERVER == 1
+        except AttributeError:
+            return False
+
+    def setup_event_loop(self) -> None:
+        try:
+            self.loop = asyncio.get_event_loop()
+            if self.loop._closed:
+                raise AttributeError
+        except Exception:
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+
+    def create_context(self) -> None:
+        self.ctx = zmq.asyncio.Context().instance()
+
+    def create_socket(self) -> None:
+        if not self.ctx:
+            self.create_context()
         self.socket = self.ctx.socket(zmq.ROUTER)
 
-        auth = ThreadAuthenticator(self.ctx)
-        auth.start()
-        auth.configure_curve_callback(domain="*", credentials_provider=self.cred_provider)
+    def set_address(self, ip: str = "*", port: int = 19080) -> None:
+        if not isinstance(ip, str):
+            raise TypeError(EXCEPTION_IP_NOT_TYPE_STR)
+
+        if not isinstance(port, int):
+            raise TypeError(EXCEPTION_PORT_NOT_TYPE_INT)
+
+        self.address = f'tcp://{ip}:{port}'
+
+    def connect_socket(self) -> None:
+        if not self.address:
+            raise AttributeError(EXCEPTION_NO_ADDRESS_SET)
+
+        if not self.socket:
+            raise AttributeError(EXCEPTION_NO_SOCKET)
+
+        self.socket.bind(self.address)
+
+    def setup_authentication_keys(self) -> None:
+        if not self.socket:
+            raise AttributeError(EXCEPTION_NO_SOCKET)
 
         self.socket.curve_secretkey = self.wallet.curve_sk
         self.socket.curve_publickey = self.wallet.curve_vk
         self.socket.curve_server = True  # must come before bind
 
-        self.socket.bind(self.address)
+    def setup_authentication(self) -> None:
+        if not self.ctx:
+            self.create_context()
+
+        self.auth = AsyncioAuthenticator(self.ctx)
+        self.auth.start()
+        self.auth.configure_curve_callback(domain="*", credentials_provider=self.credentials_provider)
+
+    def create_poller(self) -> None:
+        if not self.socket:
+            raise AttributeError(EXCEPTION_NO_SOCKET)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
+
+    def start(self) -> None:
+        print(f'[{self.log.name}][ROUTER] Starting on: ' + self.address)
+        self.log.info('[ROUTER] Starting on: ' + self.address)
+
+        # Start an authenticator for this context.
+        self.create_context()
+        self.setup_event_loop()
+        self.create_socket()
+        self.setup_authentication_keys()
+        self.setup_authentication()
+        self.connect_socket()
+
+        asyncio.ensure_future(self.check_for_messages())
 
         # Create a poller to monitor if there is any
-        poll = zmq.Poller()
-        poll.register(self.socket, zmq.POLLIN)
-        self.poll_time = 500
 
+    async def has_message(self, timeout: int = 10) -> bool:
+        return await self.socket.poll(timeout=timeout, flags=zmq.POLLIN) > 0
+
+    async def check_for_messages(self) -> None:
         self.running = True
 
         while self.running:
             if self.is_paused:
-                sleep(0.5)
+                await asyncio.sleep(1)
             else:
-                try:
-                    sockets = dict(poll.poll(self.poll_time))
-                    # print(sockets[self.socket])
-                    if self.socket in sockets:
-                        ident, empty, msg = self.socket.recv_multipart()
+                if await self.has_message(timeout=50):
+                    peer_ident_curve_vk, empty, msg = await self.socket.recv_multipart()
 
-                        print(f'[{self.log.name}][ROUTER] received: {ident}] {msg}')
-                        self.log.info(f'[ROUTER] {ident} {msg}')
+                    print(f'[{self.log.name}][ROUTER] received: {peer_ident_curve_vk}] {msg}')
+                    self.log.info(f'[ROUTER] {peer_ident_curve_vk} {msg}')
 
-                        # print('Router received %s from %s' % (msg, ident))
-                        if self.callback is not None:
-                            self.callback(self, ident, msg)
+                    # print('Router received %s from %s' % (msg, ident))
+                    if self.callback is not None:
+                        self.callback(self, peer_ident_curve_vk=peer_ident_curve_vk, msg_obj=json.loads(msg))
 
-                except zmq.ZMQError as e:
-                    if e.errno == zmq.ETERM: # Interrupted
-                        break
+    def send_msg(self, peer_ident_curve_vk: str, msg_bytes: bytes) -> None:
+        self.socket.send_multipart([peer_ident_curve_vk, b'', msg_bytes])
 
-                    if e.errno == 38: # "Socket operation on non-socket"
-                        print (e.errno)
-                        self.stop()
-                        return
-                    raise
-
-        self.socket.close()
-
-    def send_msg(self, ident: str, msg):
-        self.socket.send_multipart([ident, b'', msg])
-
-    def pause(self):
+    def pause(self) -> None:
         self.paused = True
 
-    def unpause(self):
+    def unpause(self) -> None:
         self.paused = False
 
-    def stop(self):
-        if self.running:
-            print(f'[{self.log.name}][ROUTER] Stopping.')
-            self.log.info(f'[ROUTER] Stopping.')
-            self.running = False
+    def close_socket(self) -> None:
+        if not self.socket:
+            return
+        self.socket.close()
+
+    async def stopping(self) -> None:
+        while not self.socket_is_closed:
+            await asyncio.sleep(0)
+
+    def stop(self) -> None:
+        self.running = False
+        self.unpause()
+
+        if not self.socket:
+            return
+
+        if self.auth:
+            self.auth.stop()
+
+        if not self.socket_is_closed:
+            self.close_socket()
+            self.setup_event_loop()
+            self.loop.run_until_complete(self.stopping())
+
+        self.log.info('[ROUTER] Stopped.')
+        print(f'[{self.log.name}][ROUTER] Stopped.')
--- tests/unit/test_router.py
+++ tests/unit/test_router.py
@@ -1,8 +1,11 @@
 import unittest
 import asyncio
+import uvloop
 
-from lamden.sockets.router import Router, CredentialsProvider
+from lamden.sockets.router import Router, EXCEPTION_NO_SOCKET, EXCEPTION_NO_ADDRESS_SET, EXCEPTION_IP_NOT_TYPE_STR, EXCEPTION_PORT_NOT_TYPE_INT
 from lamden.crypto.wallet import Wallet
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 class TestRouter(unittest.TestCase):
     @classmethod
@@ -10,17 +13,25 @@ class TestRouter(unittest.TestCase):
         cls.router_wallet = Wallet()
 
     def setUp(self) -> None:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
         self.wallet = self.__class__.router_wallet
         self.router = None
         self.all_peers = []
 
     def tearDown(self) -> None:
-        pass
+        if self.router:
+            self.router.stop()
+            del self.router
+
+        loop = asyncio.get_event_loop()
+        loop.stop()
+        loop.close()
 
     def create_router(self):
         self.router = Router(
-            router_wallet=self.router_wallet,
-            get_all_peers=self.get_all_peers
+            router_wallet=self.router_wallet
         )
 
     def get_all_peers(self):
@@ -37,14 +48,155 @@ class TestRouter(unittest.TestCase):
         self.create_router()
         self.assertIsNotNone(self.router)
 
-    def test_can_create_instance(self):
+    def test_can_create_Router_instance(self):
         self.create_router()
         self.assertIsNotNone(self.router)
         self.assertIsInstance(self.router, Router)
 
-    def test_creates_credientials_provider_instnace(self):
+    def test_PROPERTY_socket_is_bound__return_TRUE(self):
         self.create_router()
-        self.assertIsNotNone(self.router.cred_provider)
-        self.assertIsInstance(self.router.cred_provider, CredentialsProvider)
+        self.router.create_socket()
+        self.router.set_address(ip='127.0.0.1', port=19000)
+
+        self.assertFalse(self.router.socket_is_bound)
+        self.router.connect_socket()
+        self.assertTrue(self.router.socket_is_bound)
+
+    def test_PROPERTY_socket_is_bound__return_FALSE(self):
+        self.create_router()
+        self.router.create_socket()
+
+        self.assertFalse(self.router.socket_is_bound)
+
+    def test_PROPERTY_socket_is_closed__return_TRUE(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.socket.close()
+
+        self.assertTrue(self.router.socket_is_closed)
+
+    def test_PROPERTY_socket_is_closed__return_FALSE(self):
+        self.create_router()
+        self.router.create_socket()
+
+        self.assertFalse(self.router.socket_is_closed)
+
+    def test_PROPERTY_curve_server_setup__return_TRUE(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.setup_authentication_keys()
+
+        self.assertTrue(self.router.curve_server_setup)
+
+    def test_PROPERTY_curve_server_setup__return_FALSE(self):
+        self.create_router()
+        self.router.create_socket()
+
+        self.assertFalse(self.router.curve_server_setup)
+
+    def test_METHOD_create_context(self):
+        self.create_router()
+
+        self.assertIsNone(self.router.ctx)
+        self.router.create_context()
+        self.assertIsNotNone(self.router.ctx)
+
+    def test_METHOD_create_socket__if_context_exists(self):
+        self.create_router()
+        self.router.create_context()
+        self.router.create_socket()
+
+        self.assertIsNotNone(self.router.socket)
+
+    def test_METHOD_create_socket__if_context_does_not_exist(self):
+        self.create_router()
+        self.router.create_socket()
+
+        self.assertIsNotNone(self.router.socket)
+
+    def test_METHOD_connect_socket(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.set_address(ip='127.0.0.1', port=19000)
+        self.router.connect_socket()
+
+        self.assertTrue(len(self.router.socket.LAST_ENDPOINT) > 0)
+
+    def test_METHOD_connect_socket__raises_AttributeError_if_no_address_set(self):
+        self.create_router()
+
+        with self.assertRaises(AttributeError) as error:
+            self.router.connect_socket()
+
+        self.assertEqual(EXCEPTION_NO_ADDRESS_SET, str(error.exception))
+
+    def test_METHOD_connect_socket__raises_AttributeError_if_socket_is_none(self):
+        self.create_router()
+        self.router.set_address(ip='127.0.0.1', port=19000)
+
+        with self.assertRaises(AttributeError) as error:
+            self.router.connect_socket()
+
+        self.assertEqual(EXCEPTION_NO_SOCKET, str(error.exception))
+
+    def test_METHOD_set_address__sets_default_values(self):
+        self.create_router()
+        self.router.address = 'testing'
+        self.router.set_address()
+
+        self.assertEqual('tcp://*:19080', self.router.address)
+
+    def test_METHOD_set_address__sets_specified_value(self):
+        self.create_router()
+        self.router.address = 'testing'
+        self.router.set_address(ip="127.0.0.1", port=1200)
+
+        self.assertEqual('tcp://127.0.0.1:1200', self.router.address)
+
+    def test_METHOD_set_address__port_not_int_raises_TypeError(self):
+        self.create_router()
+        with self.assertRaises(TypeError) as error:
+            self.router.set_address(port='1200')
+
+        self.assertEqual(EXCEPTION_PORT_NOT_TYPE_INT, str(error.exception))
+
+    def test_METHOD_set_address__ip_not_str_raises_TypeError(self):
+        self.create_router()
+        with self.assertRaises(TypeError) as error:
+            self.router.set_address(ip=1200)
+
+        self.assertEqual(EXCEPTION_IP_NOT_TYPE_STR, str(error.exception))
+
+    def test_METHOD_create_poller(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.create_poller()
+        self.assertIsNotNone(self.router.poller)
+
+    def test_METHOD_create_poller__raises_AttributeError_if_socket_is_not_created(self):
+        self.create_router()
+        with self.assertRaises(AttributeError) as error:
+            self.router.create_poller()
+
+        self.assertEqual(EXCEPTION_NO_SOCKET, str(error.exception))
+
+    def test_METHOD_setup_authentication(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.setup_authentication()
+
+    def test_METHOD_setup_authentication_keys(self):
+        self.create_router()
+        self.router.create_socket()
+        self.router.setup_authentication_keys()
+
+        self.assertTrue(self.router.curve_server_setup)
+
+    def test_METHOD_setup_authentication_keys__raises_AttributeError_if_socket_is_not_created(self):
+        self.create_router()
+        with self.assertRaises(AttributeError) as error:
+            self.router.setup_authentication_keys()
+
+        self.assertEqual(EXCEPTION_NO_SOCKET, str(error.exception))
 
 
