8bc5920835d7d45c345baa6fdc2be3d36c208e92
Nikita Tokariev <mykyta.tokariev@gmail.com>
2022-06-27 14:34:28 -0600

    use temp dir to write transaction file and move it to indicate that write was indeed complete

--- lamden/nodes/filequeue.py
+++ lamden/nodes/filequeue.py
@@ -5,7 +5,6 @@ import os
 from contracting.db.encoder import decode
 import pathlib
 from lamden.logger.base import get_logger
-from threading import Lock
 
 STORAGE_HOME = pathlib.Path().home().joinpath('.lamden')
 
@@ -15,28 +14,27 @@ class FileQueue:
     def __init__(self, root=STORAGE_HOME.joinpath('txq'), write_bytes=True):
         self.root = Path(root)
         self.root.mkdir(parents=True, exist_ok=True)
+
+        self.temp = self.root.parent.joinpath('temp_txq')
+        self.temp.mkdir(parents=True, exist_ok=True)
+
         self.log = get_logger("FILE QUEUE")
         self.file_mode = 'wb' if write_bytes else 'w'
 
-        # NOTE: temporary fix to make test_base_node_throughput work
-        self.lock = Lock()
-
     def append(self, tx):
         name = str(uuid.uuid4()) + self.EXTENSION
-        # NOTE: temporary fix to make test_base_node_throughput work
-        self.lock.acquire()
-        with open(self.root.joinpath(name), self.file_mode) as f:
+        temp_filepath = self.temp.joinpath(name)
+        final_filepath = self.root.joinpath(name)
+
+        with open(temp_filepath, self.file_mode) as f:
             f.write(tx)
-        self.lock.release()
+        os.rename(temp_filepath, final_filepath)
 
     def pop(self, idx):
         items = sorted(self.root.iterdir(), key=os.path.getmtime)
         item = items.pop(idx)
-        # NOTE: temporary fix to make test_base_node_throughput work
-        self.lock.acquire()
         with open(item) as f:
             i = decode(f.read())
-        self.lock.release()
         os.remove(item)
 
         return i
@@ -46,10 +44,15 @@ class FileQueue:
             shutil.rmtree(self.root)
         except FileNotFoundError:
             pass
+        try:
+            shutil.rmtree(self.temp)
+        except FileNotFoundError:
+            pass
 
     def refresh(self):
         self.flush()
         self.root.mkdir(exist_ok=True, parents=True)
+        self.temp.mkdir(exist_ok=True, parents=True)
 
     def __len__(self):
         try:
