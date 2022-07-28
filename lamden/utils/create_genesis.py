437ae581ad82cbccb14833fa43d56f57791b016b
Nikita Tokariev <mykyta.tokariev@gmail.com>
2022-07-28 14:26:42 -0600

    create genesis block: initial

---
+++ lamden/utils/create_genesis.py
@@ -0,0 +1,154 @@
+from argparse import ArgumentParser
+from checksumdir import dirhash
+from contracting.client import ContractingClient
+from contracting.db.driver import FSDriver, ContractDriver
+from contracting.db.encoder import decode
+from hashlib import sha3_256
+from lamden.contracts import sync
+from lamden.crypto.canonical import block_hash_from_block
+from lamden.crypto.wallet import Wallet
+from lamden.logger.base import get_logger
+from lamden.storage import BlockStorage
+from lamden.utils.legacy import BLOCK_HASH_KEY, BLOCK_NUM_HEIGHT
+from os import listdir
+from pymongo import MongoClient
+import json
+import pathlib
+import sys
+
+GENESIS_HLC = '0000-00-00T00:00:00.000000000Z_0'
+GENESIS_NUMBER = 0
+GENESIS_PREV = 64 * '0'
+
+def confirm_and_flush_blocks(bs: BlockStorage) -> bool:
+    if bs.total_blocks() > 0:
+        i = ''
+        while i != 'y' and i != 'n':
+            i = input(f'Confirm deletion of block data located under {bs.root} (y/n)\n> ')
+        if i == 'y':
+            bs.flush()
+        else:
+            return False
+    
+    return True
+
+def confirm_and_flush_state(state: FSDriver) -> bool:
+    if len(listdir(state.root)) > 0:
+        i = ''
+        while i != 'y' and i != 'n':
+            i = input(f'Confirm deletion of state data located under {state.root} (y/n)\n> ')
+        if i == 'y':
+            state.flush()
+        else:
+            return False
+
+    return True
+
+def setup_genesis_contracts(state: FSDriver) -> dict:
+    state_changes = {}
+
+    contract_driver = ContractDriver(driver=state)
+    contracting_client = ContractingClient(
+        driver=contract_driver, submission_filename=sync.DEFAULT_SUBMISSION_PATH, commit=False
+    )
+    state_changes.update(contract_driver.pending_writes)
+    contract_driver.commit()
+    contracting_client.submission_contract = contracting_client.get_contract('submission')
+
+    constitution = None
+    with open(pathlib.Path.home().joinpath('constitution.json')) as f:
+        constitution = json.load(f)
+
+    sync.setup_genesis_contracts(
+        initial_masternodes=constitution['masternodes'],
+        initial_delegates=constitution['delegates'],
+        client=contracting_client,
+        commit=False
+    )
+    state_changes.update(contract_driver.pending_writes)
+    contract_driver.commit()
+
+    return state_changes
+
+if __name__ == '__main__':
+    log = get_logger('GENESIS_BLOCK')
+    parser = ArgumentParser()
+    parser.add_argument('-k', '--key', type=str, required=True)
+    parser.add_argument('--migrate', choices=['none', 'mongo', 'filesystem'], required=True)
+    args = parser.parse_args()
+
+    founders_wallet = Wallet(seed=bytes.fromhex(args.key))
+
+    genesis_block = {
+        'hash': block_hash_from_block(GENESIS_HLC, GENESIS_NUMBER, GENESIS_PREV),
+        'number': GENESIS_NUMBER,
+        'hlc_timestamp': GENESIS_HLC,
+        'previous': GENESIS_PREV,
+        'genesis': [],
+        'origin': {
+            'signature': '',
+            'signer': ''
+        },
+        'state_hash': ''
+    }
+
+    
+    bs = BlockStorage()
+    state = FSDriver()
+
+    if not confirm_and_flush_blocks(bs):
+        log.info('Aborting')
+        sys.exit(1)
+
+    if args.migrate == 'filesystem':
+        log.info('Filling genesis block...')
+        for key in state.keys():
+            genesis_block['genesis'].append({
+                'key': key,
+                'value': state.get(key)
+            })
+    elif args.migrate == 'mongo':
+        if not confirm_and_flush_state(state):
+            log.info('Aborting')
+            sys.exit(1)
+
+        client = MongoClient()
+        log.info('Migrating state data from mongo to file-based storage and filling genesis block...')
+        for record in client.lamden.state.find({'_id': {'$nin': [BLOCK_NUM_HEIGHT, BLOCK_HASH_KEY]}}):
+            try:
+                state.set(record['_id'], decode(record['v']))
+                genesis_block['genesis'].append({
+                    'key': record['_id'],
+                    'value': decode(record['v'])
+                })
+            except ValueError as err:
+                log.error(f'Skipped key: "{record["_id"]}", due to error: {err}')
+    else:
+        if not confirm_and_flush_state(state):
+            log.info('Aborting')
+            sys.exit(1)
+
+    log.info('Setting up genesis contracts')
+    state_changes = setup_genesis_contracts(state)
+    for key, value in state_changes.items():
+        genesis_block['genesis'].append({
+            'key': key,
+            'value': value
+        })
+
+    log.info('Sorting state changes inside genesis block...')
+    genesis_block['genesis'] = sorted(genesis_block['genesis'], key=lambda d: d['key'])
+
+    log.info('Computing state storage hash...')
+    genesis_block['state_hash'] = dirhash(state.root)
+
+    log.info('Signing state changes...')
+    h = sha3_256()
+    h.update('{}'.format(genesis_block['genesis']).encode())
+    genesis_block['origin']['signer'] = founders_wallet.verifying_key
+    genesis_block['origin']['signature'] = founders_wallet.sign(h.hexdigest())
+
+    log.info('Storing genesis block...')
+    bs.store_genesis_block(genesis_block)
+
+    # TODO: set latest block hash & height?
