e1ba34cb864ba3c22205b23efbff0470dd3d2dcf
Nikita Tokariev <mykyta.tokariev@gmail.com>
2023-01-13 10:58:57 -0500

    rm no longer used files

--- INSTALL.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install python3-pip -y
-
-sudo apt-get install haveged -y
-systemctl start haveged
-systemctl enable haveged
-
-pip3 install lamden
